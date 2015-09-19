a779832ec8285242c9390f81d2f5e1a2806c0841
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-09-19 13:57:26 -0700

    vector based circular buffer

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "geometric.hpp"
 #include "linear_algebra.hpp"
 #include "math_util.hpp"
+#include "circular_buffer.hpp"
+
+using namespace math;
 
 int main(int argc, const char * argv[])
 {
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
-# gfx_dev
-
-Sandbox / playground
+experiments!
