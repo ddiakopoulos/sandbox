d6b5206c51bbac78bb1e9deef80cebfc2922c66f
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-09-19 14:18:47 -0700

    running stats class

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@
 #include "circular_buffer.hpp"
 #include "concurrent_queue.hpp"
 #include "try_locker.hpp"
+#include "running_statistics.hpp"
 
 using namespace math;
 
 int main(int argc, const char * argv[])
 {
     ConcurrentQueue<float> queue;
+    RunningStats<float> stats;
     return 0;
 }
---
+++ running_statistics.hpp
@@ -0,0 +1,115 @@
+// based on http://www.johndcook.com/blog/skewness_kurtosis/
+
+#ifndef running_stats_h
+#define running_stats_h
+
+#include <stdint.h>
+namespace math
+{
+
+template<typename T, typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+class RunningStats
+{
+    uint64_t n;
+    T M1, M2, M3, M4;
+
+public:
+
+    RunningStats()
+    {
+        clear();
+    }
+
+    friend RunningStats operator + (const RunningStats a, const RunningStats b)
+    {
+        RunningStats combined;
+
+        combined.n += a.n + b.n;
+
+        T delta = b.M1 - a.M1;
+        T delta2 = delta*delta;
+        T delta3 = delta*delta2;
+        T delta4 = delta2*delta2;
+
+        combined.M1 = (a.n*a.M1 + b.n*b.M1) / combined.n;
+
+        combined.M2 = a.M2 + b.M2 + 
+                      delta2 * a.n * b.n / combined.n;
+
+        combined.M3 = a.M3 + b.M3 + 
+                      delta3 * a.n * b.n * (a.n - b.n)/(combined.n*combined.n);
+        combined.M3 += 3.0*delta * (a.n*b.M2 - b.n*a.M2) / combined.n;
+
+        combined.M4 = a.M4 + b.M4 + delta4*a.n*b.n * (a.n*a.n - a.n*b.n + b.n*b.n) / 
+                      (combined.n*combined.n*combined.n);
+        combined.M4 += 6.0*delta2 * (a.n*a.n*b.M2 + b.n*b.n*a.M2)/(combined.n*combined.n) + 
+                      4.0*delta*(a.n*b.M3 - b.n*a.M3) / combined.n;
+
+        return combined;
+
+    }
+
+    RunningStats & operator += (const RunningStats & rhs)
+    {
+        RunningStats combined = *this + rhs;
+        *this = combined;
+        return *this;
+    }
+
+    void clear()
+    {
+        n = 0;
+        M1 = M2 = M3 = M4 = 0;
+    }
+
+    void put(T x)
+    {
+        T delta, delta_n, delta_n2, term1;
+
+        uint64_t n1 = n;
+        n++;
+        delta = x - M1;
+        delta_n = delta / n;
+        delta_n2 = delta_n * delta_n;
+        term1 = delta * delta_n * n1;
+        M1 += delta_n;
+        M4 += term1 * delta_n2 * (n*n - 3*n + 3) + 6 * delta_n2 * M2 - 4 * delta_n * M3;
+        M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
+        M2 += term1;
+    }
+
+    uint64_t num_values() const
+    {
+        return n;
+    }
+
+    T compute_mean() const
+    {
+        return M1;
+    }
+
+    T compute_variance() const
+    {
+        return M2/(n-1.0);
+    }
+
+    T compute_std_dev() const
+    {
+        return sqrt(compute_variance());
+    }
+
+    T compute_skewness() const
+    {
+        return sqrt(double(n)) * M3 / pow(M2, 1.5);
+    }
+
+    T compute_kurtosis() const
+    {
+        return double(n)*M4 / (M2*M2) - 3.0;
+    }
+
+};
+
+} // end namespace math
+ 
+#endif // end running_stats_h
