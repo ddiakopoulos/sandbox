6f8fec296a1833bd1e55751f37424192f881b1b4
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-09-19 14:03:10 -0700

    concurrent queue

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include "linear_algebra.hpp"
 #include "math_util.hpp"
 #include "circular_buffer.hpp"
-
+#include "concurrent_queue.hpp"
 using namespace math;
 
 int main(int argc, const char * argv[])
 {
+    ConcurrentQueue<float> queue;
     return 0;
 }
