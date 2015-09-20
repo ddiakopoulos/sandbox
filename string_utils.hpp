00eeff98a1667a016811c585d435a5c72f224a10
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-09-19 17:42:38 -0700

    fix

--- string_utils.hpp
+++ string_utils.hpp
@@ -2,23 +2,24 @@
 #define string_utils_h
 
 #include <string>
+#include <sstream>
 
 namespace util
 {
 
-nline std::string& trim_left(std::string & s)
+inline std::string trim_left(std::string s)
 {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
     return s;
 }
 
-inline std::string& trim_right(std::string & s)
+inline std::string trim_right(std::string s)
 {
     s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
     return s;
 }
 
-inline std::string & trim(std::string  & s)
+inline std::string trim(std::string s)
 {
     return trim_left(trim_right(s));
 }
@@ -32,7 +33,6 @@ inline std::vector<std::string> split(const std::string & s, char delim)
         list.push_back(item);
     return list;
 }
-
 
 } // end namespace util
 
