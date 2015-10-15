b50006d23091bb712e1edbe58230c1e302d71479
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-10-15 08:30:38 -0700

    voxel array

--- sketch.hpp
+++ sketch.hpp
@@ -2,6 +2,21 @@
 #define sketch_h
 
 #include <vector>
+#include "linear_algebra.hpp"
+#include "math_util.hpp"
+#include "geometric.hpp"
+
+class VoxelArray
+{
+    math::int3 size;
+    std::vector<uint32_t> voxels;
+public:
+    VoxelArray() {}
+    VoxelArray(const math::int3 & size) : size(size), voxels(size.x * size.y * size.z) {}
+    const math::int3 & get_size() const { return size; }
+    uint32_t operator[](const math::int3 & coords) const { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
+    uint32_t & operator[](const math::int3 & coords) { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
+};
 
 inline std::vector<uint16_t> crop(const std::vector<uint16_t> & image, const int imgWidth, const int imgHeight, int x, int y, int width, int height)
 {
