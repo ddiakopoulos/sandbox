fb3fc7a05a89dabee8f96ec5970e97c2cda020dd
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-10-15 08:18:18 -0700

    minor clean

--- sketch.hpp
+++ sketch.hpp
@@ -6,14 +6,12 @@
 inline std::vector<uint16_t> crop(const std::vector<uint16_t> & image, const int imgWidth, const int imgHeight, int x, int y, int width, int height)
 {
     std::vector<uint16_t> newImage(height * width);
-    
     for (int i = 0; i < height; i++)
     {
-        int srcLineId = ( i + y ) * imgWidth + x;
+        int srcLineId = (i + y) * imgWidth + x;
         int desLineId = i * width;
         memcpy(newImage.data() + desLineId, image.data() + srcLineId, width * 2);
     }
-    
     return newImage;
 }
 
@@ -22,9 +20,7 @@ inline std::vector<std::vector<uint16_t>> subdivide_grid(const std::vector<uint1
     std::vector<std::vector<uint16_t>> blocks(rowDivisor * colDivisor);
     
     for (int r = 0; r < blocks.size(); r++)
-    {
         blocks[r].resize((imgWidth / rowDivisor) * (imgHeight / colDivisor));
-    }
     
     // Does it fit?
     if (imgHeight % colDivisor == 0 && imgWidth % rowDivisor == 0)
@@ -35,12 +31,8 @@ inline std::vector<std::vector<uint16_t>> subdivide_grid(const std::vector<uint1
             int blockIdx_x = 0;
             for (int x = 0; x < imgWidth; x += imgWidth / rowDivisor)
             {
-                std::cout << "x: " << x << " y: " << y << std::endl;
                 auto & block = blocks[blockIdx_y * rowDivisor + blockIdx_x];
-                std::cout << "Block Index: " << blockIdx_y * rowDivisor + blockIdx_x << std::endl;
-                
                 block = crop(image, imgWidth, imgHeight, x, y, 8, 8);
-                
                 blockIdx_x++;
             }
             blockIdx_y++;
