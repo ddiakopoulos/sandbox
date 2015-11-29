f6ad8649d0c758cdbb3baef8fa9207ae774091f6
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-11-28 22:57:46 -0800

    begin work on euclidean app

--- main.cpp
+++ main.cpp
@@ -12,9 +12,10 @@ using namespace gfx;
 //#include "examples/ssao_app.hpp"
 //#include "examples/skydome_app.hpp"
 //#include "examples/geometry_app.hpp"
-#include "examples/terrain_app.hpp"
+//#include "examples/terrain_app.hpp"
 //#include "examples/camera_app.hpp"
 //#include "examples/sandbox_app.hpp"
+#include "examples/euclidean_app.hpp"
 
 IMPLEMENT_MAIN(int argc, char * argv[])
 {
