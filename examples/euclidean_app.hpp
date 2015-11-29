31280c2688abab7cca06c824512823c032f0b5ad
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-11-28 23:55:24 -0800

    implement euclidean algorithm generator and initial visualization attempt

--- examples/euclidean_app.hpp
+++ examples/euclidean_app.hpp
@@ -4,6 +4,52 @@ using namespace math;
 using namespace util;
 using namespace gfx;
 
+std::vector<bool> make_euclidean_rhythm(int steps, int pulses)
+{
+    std::vector<bool> pattern;
+    
+    std::function<void(int, int, std::vector<bool> &, std::vector<int> &, std::vector<int> &)> bjorklund;
+    
+    bjorklund = [&bjorklund](int level, int r, std::vector<bool> & pattern, std::vector<int> & counts, std::vector<int> & remainders)
+    {
+        r++;
+        if (level > -1)
+        {
+            for (int i=0; i < counts[level]; ++i)
+                bjorklund(level - 1, r, pattern, counts, remainders);
+            if (remainders[level] != 0)
+                bjorklund(level - 2, r, pattern, counts, remainders);
+        }
+        else if (level == -1) pattern.push_back(false);
+        else if (level == -2) pattern.push_back(true);
+    };
+    
+    if (pulses > steps || pulses == 0 || steps == 0)
+        return pattern;
+    
+    std::vector<int> counts;
+    std::vector<int> remainders;
+    
+    int divisor = steps - pulses;
+    remainders.push_back(pulses);
+    int level = 0;
+    
+    while (true)
+    {
+        counts.push_back(divisor / remainders[level]);
+        remainders.push_back(divisor % remainders[level]);
+        divisor = remainders[level];
+        level++;
+        if (remainders[level] <= 1) break;
+    }
+    
+    counts.push_back(divisor);
+    
+    bjorklund(level, 0, pattern, counts, remainders);
+    
+    return pattern;
+}
+
 struct ExperimentalApp : public GLFWApp
 {
     uint64_t frameCount = 0;
@@ -18,6 +64,10 @@ struct ExperimentalApp : public GLFWApp
     std::vector<LightObject> lights;
     
     std::unique_ptr<GlShader> simpleShader;
+    
+    std::vector<bool> euclideanPattern;
+    
+    float rotationAngle = 0.0f;
     
     ExperimentalApp() : GLFWApp(940, 720, "Sandbox App")
     {
@@ -39,19 +89,26 @@ struct ExperimentalApp : public GLFWApp
             lights[1].pose.position = float3(-25, 15, 0);
         }
         
-        for (int i = 0; i < 16; i++)
+        
+        euclideanPattern = make_euclidean_rhythm(16, 4);
+        std::rotate(euclideanPattern.rbegin(), euclideanPattern.rbegin() + 1, euclideanPattern.rend()); // Rotate right
+        std::cout << "Pattern Size: " << euclideanPattern.size() << std::endl;
+        
+        for (int i = 0; i < euclideanPattern.size(); i++)
         {
             proceduralModels.push_back(Renderable(make_icosahedron()));
         }
         
-        float r = 12.0f;
+        float r = 16.0f;
         float thetaIdx = ANVIL_TAU / proceduralModels.size();
-        auto offset = thetaIdx / 2;
+        auto offset = 0;
+        
+        std::cout << offset << std::endl;
         
         for (int t = 1; t < proceduralModels.size() + 1; t++)
         {
             auto & obj = proceduralModels[t - 1];
-            obj.pose.position = { float(r * sin((t * thetaIdx) - offset)), 0.0f, float(r * cos((t * thetaIdx) - offset))};
+            obj.pose.position = { float(r * sin((t * thetaIdx) - offset)), 4.0f, float(r * cos((t * thetaIdx) - offset))};
         }
         
         grid = RenderableGrid(1, 64, 64);
@@ -72,6 +129,15 @@ struct ExperimentalApp : public GLFWApp
     void on_update(const UpdateEvent & e) override
     {
         cameraController.update(e.timestep_ms);
+        rotationAngle += e.timestep_ms;
+        
+        for (int i = 0; i < euclideanPattern.size(); ++i)
+        {
+            auto value = euclideanPattern[i];
+            if (value)
+                proceduralModels[i].pose.orientation = make_rotation_quat_axis_angle({0, 1, 0}, 0.88f * rotationAngle);
+                
+        }
     }
     
     void on_draw() override
@@ -112,11 +178,16 @@ struct ExperimentalApp : public GLFWApp
                 simpleShader->uniform("u_lights[" + std::to_string(i) + "].color", light.color);
             }
             
+            int patternIdx = 0;
             for (const auto & model : proceduralModels)
             {
+                bool pulse = euclideanPattern[patternIdx];
                 simpleShader->uniform("u_modelMatrix", model.get_model());
                 simpleShader->uniform("u_modelMatrixIT", inv(transpose(model.get_model())));
+                if (pulse) simpleShader->uniform("u_diffuse", float3(0.7f, 0.3f, 0.3f));
+                else  simpleShader->uniform("u_diffuse", float3(0.4f, 0.4f, 0.4f));
                 model.draw();
+                patternIdx++;
             }
             
             gfx::gl_check_error(__FILE__, __LINE__);
