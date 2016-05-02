61b50fddc1f7dfc5d2bd30ac113f0e040122cf9b
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2016-05-02 11:47:46 -0700

    visual studio maintain

--- sketch.hpp
+++ sketch.hpp
@@ -8,19 +8,17 @@
 
 namespace avl
 {
-
-    class VoxelArray
-    {
-        int3 size;
-        std::vector<uint32_t> voxels;
-    public:
-        VoxelArray() {}
-        VoxelArray(const int3 & size) : size(size), voxels(size.x * size.y * size.z) {}
-        const int3 & get_size() const { return size; }
-        uint32_t operator[](const int3 & coords) const { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
-        uint32_t & operator[](const int3 & coords) { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
-    };
-    
+	class VoxelArray
+	{
+		int3 size;
+		std::vector<uint32_t> voxels;
+	public:
+		VoxelArray() {}
+		VoxelArray(const int3 & size) : size(size), voxels(size.x * size.y * size.z) {}
+		const int3 & get_size() const { return size; }
+		uint32_t operator[](const int3 & coords) const { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
+		uint32_t & operator[](const int3 & coords) { return voxels[coords.z * size.x * size.y + coords.y * size.x + coords.x]; }
+	};
 }
 
 #endif // sketch_h
