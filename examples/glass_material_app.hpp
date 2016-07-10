56d915557ce9df099dfaa901d974d153c50e0044
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2016-07-10 08:58:59 -0700

    example cleanups

--- examples/glass_material_app.hpp
+++ examples/glass_material_app.hpp
@@ -1,22 +1,16 @@
 #include "index.hpp"
 #include "stb/stb_image_write.h"
 
-//std::vector<uint8_t> data(1024 * 1024 * 3);
-//glGetTexImage(faces[i].first, 0, GL_RGB, GL_UNSIGNED_BYTE, data.data());
-//stbi_write_png(std::string(std::to_string(i) + ".png").c_str(), 1024, 1024, 3, data.data(), 1024 * 3);
 
 class CubemapCamera
 {
-
     GlFramebuffer framebuffer;
+    GlTexture colorBuffer;
     GLuint cubeMapHandle;
     float2 resolution;
     std::vector<std::pair<GLenum, Pose>> faces;
 
 public:
-
-    GlTexture colorBuffer;
-    GlTexture depthBuffer;
 
     std::function<void(float3 eyePosition, float4x4 viewMatrix, float4x4 projMatrix)> render;
 
@@ -25,9 +19,7 @@ public:
         this->resolution = resolution;
 
         colorBuffer.load_data(resolution.x, resolution.y, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-        //depthBuffer.load_data(resolution.x, resolution.y, GL_DEPTH_COMPONENT32, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
         framebuffer.attach(GL_COLOR_ATTACHMENT0, colorBuffer);
-        //framebuffer.attach(GL_DEPTH_ATTACHMENT, depthBuffer);
         if (!framebuffer.check_complete()) throw std::runtime_error("incomplete framebuffer");
         
         gl_check_error(__FILE__, __LINE__);
@@ -51,17 +43,19 @@ public:
 
         std::vector<float3> targets = {{1, 0, 0,}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
         std::vector<float3> upVecs = {{0, -1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, 1}, {0, -1, 0}, {0, -1, 0}};
-         for (int i = 0; i < 6; ++i)
-         {
-             auto f = std::pair<GLenum, Pose>(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, look_at_pose(float3(0, 0, 0), targets[i], upVecs[i]));
-             faces.push_back(f);
-         }
-        
+        for (int i = 0; i < 6; ++i)
+        {
+            auto f = std::pair<GLenum, Pose>(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, look_at_pose(float3(0, 0, 0), targets[i], upVecs[i]));
+            faces.push_back(f);
+        }
+     
         gl_check_error(__FILE__, __LINE__);
-    }
+     }
 
-    void update(float3 eyePosition)
-    {
+     GLuint get_cubemap_handle() const { return cubeMapHandle; }
+
+     void update(float3 eyePosition)
+     {
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, framebuffer.get_handle());
         glViewport(0, 0, resolution.x , resolution.y);
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -72,14 +66,11 @@ public:
         {
             glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, faces[i].first, cubeMapHandle, 0);
             auto viewMatrix = make_view_matrix_from_pose(faces[i].second);
-
             if (render) render(eyePosition, viewMatrix, projMatrix);
         }
 
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
     }
-
-    GLuint get_cubemap_handle() const { return cubeMapHandle; }
 };
 
 std::shared_ptr<GlShader> make_watched_shader(ShaderMonitor & mon, const std::string vertexPath, const std::string fragPath)
@@ -224,14 +215,14 @@ struct ExperimentalApp : public GLFWApp
         int width, height;
         glfwGetWindowSize(window, &width, &height);
         glViewport(0, 0, width, height);
-     
+        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(1.0f, 0.1f, 0.0f, 1.0f);
 
         const auto proj = camera.get_projection_matrix((float) width / (float) height);
         const float4x4 view = camera.get_view_matrix();
         const float4x4 viewProj = mul(proj, view);
-       
+        
         auto draw_cubes = [&](float3 eye, float4x4 vp, float3 emissive)
         {
             simpleShader->bind();
@@ -293,15 +284,11 @@ struct ExperimentalApp : public GLFWApp
         }
 
         draw_cubes(camera.get_eye_point(), viewProj, float3(0, 0, 0));
-
         grid.render(proj, view);
 
         gl_check_error(__FILE__, __LINE__);
-
         if (igm) igm->end_frame();
-
         glfwSwapBuffers(window);
-        
         frameCount++;
     }
     
