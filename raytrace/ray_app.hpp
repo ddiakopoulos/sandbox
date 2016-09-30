3cb821e9cc450608ed6f9a2b4f39cf2121396314
dimitri <ddiakopoulos@gmail.com>
2016-09-30 13:31:40 -0700

    diffuse reflected rays

--- raytrace/ray_app.hpp
+++ raytrace/ray_app.hpp
@@ -6,7 +6,7 @@
 // ToDo
 // ----------------------------------------------------------------------------
 // [X] Decouple window size / framebuffer size for gl render target
-// [X] Raytraced scene - spheres with phong shading
+// [X] Whitted Raytraced scene - spheres with phong shading
 // [X] Occlusion support
 // [X] ImGui Controls
 // [ ] Add other objects (box, plane, disc)
@@ -29,15 +29,30 @@ struct Material
     float3 diffuse = { 0, 0, 0 };
     float3 emissive = { 0, 0, 0 };
 
+    // Math borrowed from Kevin Beason's `smallpt`
     Ray get_reflected_ray(const Ray & r, const float3 & p, const float3 & n)
     {
-        // perfect specular reflection
+        /*
+        // ideal specular reflection
         float roughness = 0.925;
         float3 refl = r.direction - n * 2.0f * dot(n, r.direction);
         refl = normalize(float3(refl.x + (gen.random_float() - 0.5f) * roughness,
                                 refl.y + (gen.random_float() - 0.5f) * roughness,
                                 refl.z + (gen.random_float() - 0.5f) * roughness));
         return Ray(p, refl);
+        */
+
+        // ideal diffuse reflection
+        float3 NdotL = dot(n, r.direction) < 0.0f ? n : n * -1.f; // orient the surface normal
+        float r1 = 2 * ANVIL_PI * gen.random_float(); // random angle around a hemisphere
+        float r2 = gen.random_float();
+        float r2s = sqrt(r2); // distance from center
+
+        // u is perpendicular to w && v is perpendicular to u and w
+        float3 u = normalize(cross((abs(NdotL.x) > .10 ? float3(0, 1, 0) : float3(1, 1, 1)), NdotL));   
+        float3 v = cross(NdotL, u);
+        float3 d = normalize(u * cos(r1) * r2s + v * std::sin(r1) * r2s + NdotL * std::sqrt(1.f - r2)); // random reflection ray
+        return Ray(p, d);
     }
 };
 
@@ -254,7 +269,7 @@ struct ExperimentalApp : public GLFWApp
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.f, 0.f, 0.f, 1.0f);
 
-        const float numSamples = 16.f;
+        const float numSamples = 1.f;
 
         for (int y = 0; y < film->size.y; ++y)
         {
