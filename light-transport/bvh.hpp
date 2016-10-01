9162742381b42a9e1f068b784d9a0132d382ac4c
dimitri <ddiakopoulos@gmail.com>
2016-10-01 13:00:52 -0700

    stub struct for comparing traceable objects for splitting up bvh nodes

--- light-transport/bvh.hpp
+++ light-transport/bvh.hpp
@@ -11,20 +11,42 @@ using namespace avl;
 
 class BVH
 {
+
+	struct ObjectComparator
+	{
+		int split;
+		ObjectComparator(const int axis = 0) : split(axis) { }
+		bool operator()(const Traceable * a, const Traceable * b) const { return true; }
+	};
+
 	std::vector<std::shared_ptr<Traceable>> objects;
-	bool initialized;
+	bool initialized = false;
 public:
 
 	BVH(std::vector<std::shared_ptr<Traceable>> objects) : objects(objects) {}
-	~BVH();
+	~BVH() {}
 
-	void build();
+	void build()
+	{
+		// ... 
+		initialized = true;
+	}
 
 	// Compute entire bounds of BVH in world space
-	Bounds3D world_space() const;
+	Bounds3D world_space() const
+	{
 
-	RayIntersection intersect(const Ray & ray);
-	RayIntersection intersect_p(const Ray & ray);
+	}
+
+	RayIntersection intersect(const Ray & ray)
+	{
+
+	}
+
+	RayIntersection intersect_p(const Ray & ray)
+	{
+
+	}
 };
 
 #endif
