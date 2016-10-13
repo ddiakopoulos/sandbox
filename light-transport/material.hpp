eaf7a01d825f8a50249be15f3ca0be1739a97bbd
dimitri <ddiakopoulos@gmail.com>
2016-10-13 11:07:19 -0700

    additional material comments

--- light-transport/material.hpp
+++ light-transport/material.hpp
@@ -31,6 +31,16 @@ inline float3 sample_hemisphere(const float3 & N, const UniformRandomGenerator &
 	return normalize(u * std::cos(r1) * r2s + v * std::sin(r1) * r2s + w * std::sqrt(1.0f - r2));
 }
 
+// BRDF Lexicon
+// ============================================================================
+// P  = point of ray intersection
+// N  = surface normal at P
+// Wo = vector pointing in the opposite direction of the incident ray
+// Wr = reflected vector
+// Wt = transmitted vector
+// We = emitted vector
+// ============================================================================
+
 struct Material
 {
 	float3 Kd = { 0, 0, 0 }; // diffuse
@@ -39,16 +49,14 @@ struct Material
 	// Reflected
 	void bsdf_Wr(const float3 & P, const float3 & N, const float3 & Wr, const float3 & Wt, const float3 & Wo, const UniformRandomGenerator & gen, float & brdf, float & btdf)
 	{
-		float c = dot(Wr, N);
-		brdf = ANVIL_INV_PI * c;
+		brdf = ANVIL_INV_PI * dot(Wr, N);
 		btdf = 0.0f;
 	}
 
 	// Emitted
 	void bsdf_We(const float3 & P, const float3 & N, const float3 & We, const float3 & Wr, const float3 & Wt, const float3 & Wo, const UniformRandomGenerator & gen, float & brdf, float & btdf)
 	{
-		float c = std::max(dot(We, N), 0.0f);
-		brdf = ANVIL_INV_PI * c;
+		brdf = ANVIL_INV_PI * std::max(dot(We, N), 0.0f);
 		btdf = 0.0f;
 	}
 
@@ -58,10 +66,10 @@ struct Material
 		return 1.f / ANVIL_TWO_PI;
 	}
 
-	// Evaluate indicent vector
+	// Sample the indicent vector to produce a reflected vector
 	void sample_Wi(const float3 & Wo, const float3 & N, const UniformRandomGenerator & gen, float3 & Wr, float3 & Wt)
 	{
-		Wr = sample_hemisphere(N, gen);
+		Wr = sample_hemisphere(N, gen); // sample the normal direction
 		Wt = float3(0, 0, 0); // no transmission
 	}
 
