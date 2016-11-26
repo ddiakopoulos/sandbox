b492ce418facfc88426aaec86055e9a28d78d520
dimitri <ddiakopoulos@gmail.com>
2016-11-26 10:24:56 -0800

    spend 5 mins fixing longstanding issues with arcball

--- arcball.hpp
+++ arcball.hpp
@@ -11,75 +11,79 @@
 namespace avl
 {
 
-    // Force sphere point to be perpendicular to axis
-    inline float3 constrain_to_axis(const float3 & loose, const float3 & axis)
-    {
-        float norm;
-        float3 onPlane = loose - axis * dot(axis, loose);
-        norm = length2(onPlane);
-        
-        if (norm > 0.0f)
-        {
-            if (onPlane.z < 0.0f) onPlane = -onPlane;
-            return (onPlane * (1.0f / std::sqrt(norm)));
-        }
-        
-        if (dot(axis, float3(0, 0, 1)) < 0.0001f) onPlane = float3(1, 0, 0);
-        else onPlane = normalize(float3(-axis.y, axis.x, 0));
-        
-        return onPlane;
-    }
-    
-    struct ArcballCamera
-    {
-        float2 windowSize;
-        float2 initialMousePos;
-        float4 initialQuat, currentQuat;
-        float3 constraintAxis = {0, 0, 0};
-        
-        ArcballCamera(float2 windowSize) : windowSize(windowSize) { initialQuat = currentQuat = float4(0, 0, 0, 1); }
-        
-        void mouse_down(const float2 & mousePos)
-        {
-            initialMousePos = mousePos;
-            initialQuat = float4(0, 0, 0, 1);
-        }
-        
-        void mouse_drag(const float2 & mousePos)
-        {
-            auto a = mouse_on_sphere(initialMousePos);
-            auto b = mouse_on_sphere(mousePos);
-            
-            if (length(constraintAxis))
-            {
-                a = constrain_to_axis(a, constraintAxis);
-                b = constrain_to_axis(b, constraintAxis);
-            }
-        
-            auto rotation = safe_normalize(make_rotation_quat_between_vectors(a, b)); // singularity case?
-            currentQuat = safe_normalize(qmul(initialQuat, rotation));
-        }
-        
-        float3 mouse_on_sphere(const float2 & mouse)
-        {
-            float3 result = {0, 0, 0};
-            result.x = (mouse.x - (0.5f * windowSize.x)) / (0.5f * windowSize.x);
-            result.y = -(mouse.y - (0.5f * windowSize.y)) / (0.5f * windowSize.y);
-            
-            float mag = length2(result);
-            
-            if (mag > 1.0f) result = normalize(result);
-            else
-            {
-                result.z = sqrtf(1.0 - mag);
-                result = normalize(result);
-            }
-            
-            return result;
-        }
-        
-    };
-    
+	// Force sphere point to be perpendicular to axis
+	inline float3 constrain_to_axis(const float3 & loose, const float3 & axis)
+	{
+		float norm;
+		float3 onPlane = loose - axis * dot(axis, loose);
+		norm = length2(onPlane);
+
+		if (norm > 0.0f)
+		{
+			if (onPlane.z < 0.0f) onPlane = -onPlane;
+			return (onPlane * (1.0f / std::sqrt(norm)));
+		}
+
+		if (dot(axis, float3(0, 0, 1)) < 0.0001f) onPlane = float3(1, 0, 0);
+		else onPlane = normalize(float3(-axis.y, axis.x, 0));
+
+		return onPlane;
+	}
+
+	struct ArcballCamera
+	{
+		float2 windowSize;
+		float2 initialMousePos;
+		float4 initialQuat, currentQuat;
+		float3 constraintAxis = { 0, 0, 0 };
+
+		ArcballCamera(float2 windowSize) : windowSize(windowSize) { initialQuat = currentQuat = float4(0, 0, 0, 1); }
+
+		void mouse_down(const float2 & mousePos)
+		{
+			initialMousePos = mousePos;
+			initialQuat = float4(0, 0, 0, 1);
+		}
+
+		void mouse_drag(const float2 & mousePos)
+		{
+			auto a = mouse_on_sphere(initialMousePos);
+			auto b = mouse_on_sphere(mousePos);
+
+			if (length(constraintAxis))
+			{
+				a = constrain_to_axis(a, constraintAxis);
+				b = constrain_to_axis(b, constraintAxis);
+			}
+
+			if (a == b) return;
+
+			auto rotation = normalize(make_rotation_quat_between_vectors(a, b));
+			auto deltaRotation = normalize(qmul(rotation, qconj(initialQuat)));
+			currentQuat = deltaRotation;
+			initialMousePos = mousePos; // delta rotation
+		}
+
+		float3 mouse_on_sphere(const float2 & mouse)
+		{
+			float3 result = { 0, 0, 0 };
+			result.x = (mouse.x - (0.5f * windowSize.x)) / (0.5f * windowSize.x);
+			result.y = -(mouse.y - (0.5f * windowSize.y)) / (0.5f * windowSize.y);
+
+			float mag = length2(result);
+
+			if (mag > 1.0f) result = normalize(result);
+			else
+			{
+				result.z = sqrtf(1.0 - mag);
+				result = normalize(result);
+			}
+
+			return result;
+		}
+
+	};
+
 }
 
 #endif // end arcball_h
