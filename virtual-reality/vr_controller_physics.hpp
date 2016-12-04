459f30e2d3e1616105441259ec81d5249918fb20
dimitri <ddiakopoulos@gmail.com>
2016-12-04 15:14:08 -0800

    sketch out necessary solvers and world... will bite me that I'm not setting up for multiple worlds

--- virtual-reality/vr_controller_physics.hpp
+++ virtual-reality/vr_controller_physics.hpp
@@ -4,11 +4,40 @@
 #define vr_controller_physics_hpp
 
 #include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
+
 #include "linalg_util.hpp"
 #include "geometric.hpp"
 #include "geometry.hpp"
 #include "bullet_utils.hpp"
+#include <functional>
+#include <vector>
 
 using namespace avl;
+
+class BulletEngineVR
+{
+	using OnTickCallback = std::function<void(float, BulletEngineVR *)>;
+
+	btBroadphaseInterface * broadphase = { nullptr };
+	btDefaultCollisionConfiguration* collisionConfiguration = { nullptr };
+	btCollisionDispatcher * dispatcher = { nullptr };
+	btSequentialImpulseConstraintSolver * solver = { nullptr };
+	btDiscreteDynamicsWorld * dynamicsWorld = { nullptr };
+
+	std::vector<std::pair<OnTickCallback, int>> bulletTicks;
+
+public:
+	BulletEngineVR()
+	{
+
+	}
+
+	~BulletEngineVR()
+	{
+
+	}
+};
+
 
 #endif
