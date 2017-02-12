d165a826c34addcdefb9891efb714149addddc29
dimitri <ddiakopoulos@gmail.com>
2017-02-12 13:08:10 -0800

    first contact implementation for bullet objects

--- virtual-reality/vr_controller_physics.hpp
+++ virtual-reality/vr_controller_physics.hpp
@@ -80,7 +80,7 @@ struct CollideCallback : public btCollisionWorld::ContactResultCallback
 	CollideCallback(btRigidBody & target) : btCollisionWorld::ContactResultCallback(), body(target) { }
 
 	// Called for each contact point
-	virtual btScalar addSingleResult(btManifoldPoint & cp, const btCollisionObjectWrapper * colObj0, int, int, const btCollisionObjectWrapper * colObj1, int, int)
+	virtual btScalar addSingleResult(btManifoldPoint & cp, const btCollisionObjectWrapper * colObj0, int, int, const btCollisionObjectWrapper * colObj1, int, int) override
 	{
 		btVector3 point, n;
 		BulletContactPointVR p;
@@ -101,7 +101,7 @@ struct CollideCallback : public btCollisionWorld::ContactResultCallback
 		p.location = { point.x(), point.y(), point.z() };
 		p.normal = { n.x(), n.y(), n.z() };
 		p.depth = fabs(cp.getDistance());
-		p.velocity = { body.getLinearVelocity().x, body.getLinearVelocity().y, body.getLinearVelocity().z };
+		p.velocity = { body.getLinearVelocity().x(), body.getLinearVelocity().y(), body.getLinearVelocity().z() };
 		p.velocityNorm = dot(p.normal, p.velocity);
 
 		contacts.push_back(p);
@@ -136,8 +136,8 @@ public:
 	{
 		struct CollideCallbackWorld : public CollideCallback
 		{
-			CollideCallbackWorld(btRigidBody & target) : CollideCallback(target) { }
-			virtual bool needsCollision(btBroadphaseProxy * proxy) const
+			CollideCallbackWorld(btRigidBody & target) : CollideCallback(target) {}
+			virtual bool needsCollision(btBroadphaseProxy * proxy) const override
 			{
 				if (!btCollisionWorld::ContactResultCallback::needsCollision(proxy)) return false; // superclass pre-filters
 				return body.checkCollideWithOverride(static_cast<btCollisionObject*>(proxy->m_clientObject)); // avoid contraint contacts
@@ -146,7 +146,6 @@ public:
 
 		CollideCallbackWorld callback(*body.get());
 		world->contactTest(body.get(), callback);
-
 		return callback.contacts;
 	}
 
@@ -159,7 +158,35 @@ public:
 
 	bool FirstContact(const float4x4 & src, BulletContactPointVR & contact) const
 	{
+		struct ContactCallback : public btCollisionWorld::ClosestConvexResultCallback
+		{
+			btRigidBody & me;
+			BulletContactPointVR point;
+			bool hit{ false };
 
+			ContactCallback(btRigidBody & me, const float3 & to) : btCollisionWorld::ClosestConvexResultCallback(me.getWorldTransform().getOrigin(), btVector3(to[0], to[1], to[2])), me(me) {}
+
+			bool needsCollision(btBroadphaseProxy* proxy) const
+			{
+				if (proxy->m_clientObject == &me) return false;
+				if (!btCollisionWorld::ClosestConvexResultCallback::needsCollision(proxy)) return false;
+				return me.checkCollideWithOverride(static_cast<btCollisionObject*>(proxy->m_clientObject));
+			}
+
+			btScalar addSingleResult(btCollisionWorld::LocalConvexResult& convexResult, bool)
+			{
+				point.depth = convexResult.m_hitFraction;
+				point.normal = float3(convexResult.m_hitNormalLocal.x(), convexResult.m_hitNormalLocal.y(), convexResult.m_hitNormalLocal.z());
+				point.object = const_cast<btCollisionObject*>(convexResult.m_hitCollisionObject);
+				hit = true;
+				return 0;
+			}
+		};
+
+		ContactCallback callback(*body.get(), src.w.xyz());
+		world->convexSweepTest(reinterpret_cast<btConvexShape*>(body->getCollisionShape()), body->getWorldTransform(), to_bt(src), callback, 0);
+		contact = callback.point;
+		return callback.hit;
 	}
 
 	~BulletObjectVR()
