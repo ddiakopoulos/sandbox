0846a0da30caafdd3533ec1e2e473087ba19ff86
dimitri <ddiakopoulos@gmail.com>
2017-02-25 10:53:42 -0800

    reorganize

---
+++ virtual-reality/renderer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef vr_renderer_hpp
+#define vr_renderer_hpp
+
+#include "linalg_util.hpp"
+#include "geometric.hpp"
+#include "geometry.hpp"
+
+using namespace avl;
+
+
+#endif // end vr_renderer_hpp
