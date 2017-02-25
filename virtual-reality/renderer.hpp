87d1b5a9dfede94508cb94dddbe8872e97c93908
dimitri <ddiakopoulos@gmail.com>
2017-02-25 11:08:28 -0800

    begin sketching out render interface

--- virtual-reality/renderer.hpp
+++ virtual-reality/renderer.hpp
@@ -6,8 +6,32 @@
 #include "linalg_util.hpp"
 #include "geometric.hpp"
 #include "geometry.hpp"
+#include "camera.hpp"
 
 using namespace avl;
 
+struct RenderSet
+{
+
+};
+
+class Renderer
+{
+	std::vector<RenderSet *> renderSets;
+	GlCamera * firstPersonCamera;
+	GlCamera * debugCamera;
+
+public:
+
+	Renderer();
+	~Renderer();
+
+	void set_cameras(GlCamera * fpsCam, GlCamera * debugCam);
+	GlCamera * get_active_camera();
+
+
+
+
+};
 
 #endif // end vr_renderer_hpp
