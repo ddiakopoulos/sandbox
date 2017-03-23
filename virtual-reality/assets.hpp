e41a982647b87f9b3e65fc8be8387685c0848d66
dimitri <ddiakopoulos@gmail.com>
2017-03-23 05:51:32 -0700

    assets can be retrieved by shared ptr

--- virtual-reality/assets.hpp
+++ virtual-reality/assets.hpp
@@ -3,22 +3,30 @@
 #ifndef vr_assets_hpp
 #define vr_assets_hpp
 
-#include "GL_API.hpp"
+#include "util.hpp"
 #include "linalg_util.hpp"
-#include "geometric.hpp"
+#include <memory>
+
+template<typename T>
+struct UniqueAsset : public Noncopyable
+{
+    std::string name;
+    T asset;
+    UniqueAsset(const std::string name, T & asset) : name(name), asset(std::move(asset)) {}
+};
 
 template<typename T>
 class AssetDatabase
 {
-    std::map<std::string, T> table;
+    std::map<std::string, std::shared_ptr<UniqueAsset<T>>> table;
 public:
     void register_asset(const std::string & name, T && asset)
     {
-        table[name] = std::move(asset);
+        table[name] = std::make_shared<UniqueAsset<T>>(name, std::move(asset));
     }
     T & get_asset(const std::string & name)
     {
-        return table[name];
+        return table[name]->asset;
     }
 };
 
