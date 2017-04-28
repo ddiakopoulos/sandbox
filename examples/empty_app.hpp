0c2c4c43469b29d98564c0346ffd42821ebe40d8
dimitri <ddiakopoulos@gmail.com>
2017-04-28 08:58:10 -0700

    integrate coordinate system helpers from Sterling's engine.h project

--- examples/empty_app.hpp
+++ examples/empty_app.hpp
@@ -21,8 +21,35 @@ constexpr const char basic_wireframe_frag[] = R"(#version 330
     }
 )";
 
+// A value type representing an abstract direction vector in 3D space, independent of any coordinate system
+enum class coord_axis { forward, back, left, right, up, down };
+
+inline float dot(coord_axis a, coord_axis b)
+{
+    static float table[6][6]{ { +1,-1,0,0,0,0 },{ -1,+1,0,0,0,0 },{ 0,0,+1,-1,0,0 },{ 0,0,-1,+1,0,0 },{ 0,0,0,0,+1,-1 },{ 0,0,0,0,-1,+1 } };
+    return table[static_cast<int>(a)][static_cast<int>(b)];
+}
+
+// A concrete 3D coordinate system with defined x, y, and z axes
+struct coord_system
+{
+    coord_axis x_axis, y_axis, z_axis;
+    float3 get_axis(coord_axis a) const { return{ dot(x_axis, a), dot(y_axis, a), dot(z_axis, a) }; }
+    float3 get_left() const { return get_axis(coord_axis::left); }
+    float3 get_right() const { return get_axis(coord_axis::right); }
+    float3 get_up() const { return get_axis(coord_axis::up); }
+    float3 get_down() const { return get_axis(coord_axis::down); }
+    float3 get_forward() const { return get_axis(coord_axis::forward); }
+    float3 get_back() const { return get_axis(coord_axis::back); }
+};
+
+inline float3x3 make_transform(const coord_system & from, const coord_system & to) { return{ to.get_axis(from.x_axis), to.get_axis(from.y_axis), to.get_axis(from.z_axis) }; }
+inline float4x4 make_transform_4x4(const coord_system & from, const coord_system & to) { return{ { to.get_axis(from.x_axis),0 },{ to.get_axis(from.y_axis),0 },{ to.get_axis(from.z_axis),0 },{ 0,0,0,1 } }; }
+
 inline Geometry coordinate_system_geometry()
 {
+    coord_system opengl_coords { coord_axis::right, coord_axis::up, coord_axis::back }; // traditional rh opengl
+
     Geometry axis;
 
     axis.vertices.emplace_back(0.f, 0.f, 0.f);
@@ -38,13 +65,6 @@ inline Geometry coordinate_system_geometry()
     axis.colors.emplace_back(0.f, 1.f, 0.f, 1.f);
     axis.colors.emplace_back(1.f, 0.f, 0.f, 1.f);
     axis.colors.emplace_back(1.f, 0.f, 0.f, 1.f);
-
-    axis.normals.emplace_back(0.f, 0.f, 1.f);
-    axis.normals.emplace_back(0.f, 0.f, 1.f);
-    axis.normals.emplace_back(0.f, 1.f, 0.f);
-    axis.normals.emplace_back(0.f, 1.f, 0.f);
-    axis.normals.emplace_back(1.f, 0.f, 0.f);
-    axis.normals.emplace_back(1.f, 0.f, 0.f);
 
     return axis;
 }
