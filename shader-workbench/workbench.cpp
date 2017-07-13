002d16ebc375b97bb0f5d4474a4363d6e9555a10
dimitri <ddiakopoulos@gmail.com>
2017-07-12 23:21:20 -0700

    test with two bone ik derived from unreal source

--- shader-workbench/workbench.cpp
+++ shader-workbench/workbench.cpp
@@ -4,76 +4,17 @@
 
 using namespace avl;
 
-Geometry make_perlin_mesh(int gridSize = 32.f)
+struct IKChain
 {
-    Geometry terrain;
+    Pose root;
+    Pose joint;
+    Pose end;
+};
 
-    for (int x = 0; x <= gridSize; x++)
-    {
-        for (int z = 0; z <= gridSize; z++)
-        {
-            float y = ((noise::noise(float2(x * 0.1f, z * 0.1f))) + 1.0f) / 2.0f;
-            y = y * 3.0f;
-            terrain.vertices.push_back({ (float)x, (float)y, (float)z });
-        }
-    }
-
-    std::vector<uint4> quads;
-    for (int x = 0; x < gridSize; ++x)
-    {
-        for (int z = 0; z < gridSize; ++z)
-        {
-            std::uint32_t tlIndex = z * (gridSize + 1) + x;
-            std::uint32_t trIndex = z * (gridSize + 1) + (x + 1);
-            std::uint32_t blIndex = (z + 1) * (gridSize + 1) + x;
-            std::uint32_t brIndex = (z + 1) * (gridSize + 1) + (x + 1);
-            quads.push_back({ blIndex, tlIndex, trIndex, brIndex });
-        }
-    }
-
-    for (auto f : quads)
-    {
-        terrain.faces.push_back(uint3(f.x, f.y, f.z));
-        terrain.faces.push_back(uint3(f.x, f.z, f.w));
-    }
-
-    terrain.compute_normals(false);
-
-    return terrain;
-}
-
-inline GlMesh fullscreen_quad_extra(const float4x4 & projectionMatrix, const float4x4 & viewMatrix)
-{
-    // Camera position is reconstructed in the shader, but we still need the correct orientation 
-    float4x4 viewMatrixNoTranslation = viewMatrix;
-    viewMatrixNoTranslation.w = float4(0, 0, 0, 1);
-
-    // Extract the frustum coordinates of the far clip plane in ndc space
-    float3 frustumVerts[8] = {
-        { -1.f, -1.f, 1.f},
-        { -1.f, +1.f, 1.f},
-        { +1.f, +1.f, 1.f},
-        { +1.f, -1.f, 1.f},
-    };
-
-    for (unsigned int j = 0; j < 8; ++j) frustumVerts[j] = transform_coord(inverse(mul(projectionMatrix, viewMatrixNoTranslation)), frustumVerts[j]);
-
-    struct Vertex { float3 position; float2 texcoord; float3 ray; };
-    const float3 verts[6] = { { -1.0f, -1.0f, 0.0f },{ 1.0f, -1.0f, 0.0f },{ -1.0f, 1.0f, 0.0f },{ -1.0f, 1.0f, 0.0f },{ 1.0f, -1.0f, 0.0f },{ 1.0f, 1.0f, 0.0f } };
-    const float2 texcoords[6] = { { 0, 0 },{ 1, 0 },{ 0, 1 },{ 0, 1 },{ 1, 0 },{ 1, 1 } };
-    const float3 rayCoords[6] = { frustumVerts[0], frustumVerts[3], frustumVerts[1], frustumVerts[1], frustumVerts[3], frustumVerts[2] };
-    const uint3 faces[2] = { { 0, 1, 2 },{ 3, 4, 5 } };
-    std::vector<Vertex> vertices;
-    for (int i = 0; i < 6; ++i) vertices.push_back({ verts[i], texcoords[i], rayCoords[i] });
-
-    GlMesh mesh;
-    mesh.set_vertices(vertices, GL_STATIC_DRAW);
-    mesh.set_attribute(0, &Vertex::position);
-    mesh.set_attribute(1, &Vertex::texcoord);
-    mesh.set_attribute(2, &Vertex::ray);
-    mesh.set_elements(faces, GL_STATIC_DRAW);
-    return mesh;
-}
+tinygizmo::rigid_transform root_transform;
+tinygizmo::rigid_transform joint_transform;
+tinygizmo::rigid_transform end_transform;
+tinygizmo::rigid_transform target_transform;
 
 shader_workbench::shader_workbench() : GLFWApp(1200, 800, "Shader Workbench")
 {
@@ -84,39 +25,18 @@ shader_workbench::shader_workbench() : GLFWApp(1200, 800, "Shader Workbench")
     igm.reset(new gui::ImGuiManager(window));
     gui::make_dark_theme();
 
-    terrainScan = shaderMonitor.watch("../assets/shaders/terrainscan_vert.glsl", "../assets/shaders/terrainscan_frag.glsl");
     normalDebug = shaderMonitor.watch("../assets/shaders/normal_debug_vert.glsl", "../assets/shaders/normal_debug_frag.glsl");
-    triplanarTexture = shaderMonitor.watch("../assets/shaders/triplanar_vert.glsl", "../assets/shaders/triplanar_frag.glsl");
-    projector.projectorMultiplyShader = shaderMonitor.watch("../assets/shaders/projector_multiply_vert.glsl", "../assets/shaders/projector_multiply_frag.glsl");
 
-    terrainMesh = make_mesh_from_geometry(make_perlin_mesh(16));
+    //terrainMesh = make_mesh_from_geometry(make_perlin_mesh(16));
 
-    sceneColorTexture.setup(width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-    sceneDepthTexture.setup(width, height, GL_DEPTH_COMPONENT32, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
-    glNamedFramebufferTexture2DEXT(sceneFramebuffer, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sceneColorTexture, 0);
-    glNamedFramebufferTexture2DEXT(sceneFramebuffer, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, sceneDepthTexture, 0);
-    sceneFramebuffer.check_complete();
-
-    rustyTexture = load_image("../assets/textures/pbr/rusted_iron_2048/albedo.png", true);
-    topTexture = load_image("../assets/nonfree/diffuse.png", true);
-    glTextureParameteriEXT(rustyTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteriEXT(rustyTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTextureParameteriEXT(topTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTextureParameteriEXT(topTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    projector.cookieTexture = std::make_shared<GlTexture2D>(load_image("../assets/textures/projector/hexagon_select.png", false));
-    glTextureParameteriEXT(*projector.cookieTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTextureParameteriEXT(*projector.cookieTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-    projector.gradientTexture = std::make_shared<GlTexture2D>(load_image("../assets/textures/projector/gradient.png", false));
-    glTextureParameteriEXT(*projector.gradientTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTextureParameteriEXT(*projector.gradientTexture, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    sphere_mesh = make_sphere_mesh(0.25f);
 
     gizmo.reset(new GlGizmo());
 
-    projector.pose = look_at_pose_rh({ 0.1f, 4.0, 0.1f }, { 0, 0.1f, 0 });
-    std::cout << "Cookie: " << *projector.cookieTexture << std::endl;
-    std::cout << "Gradient: " << *projector.gradientTexture << std::endl;
+    root_transform.position.y = 1.f;
+    joint_transform.position.y = 0.5f;
+    joint_transform.position.z = -0.15f;
+    end_transform.position.y = 0.f;
 
     cam.look_at({ 0, 9.5f, -6.0f }, { 0, 0.1f, 0 });
     flycam.set_camera(&cam);
@@ -152,20 +72,6 @@ void shader_workbench::on_update(const UpdateEvent & e)
     elapsedTime += e.timestep_ms;
 }
 
-static float3 scale = float3(0.25);
-static bool renderColor = true;
-static bool renderProjective = true;
-static float3 projectorEye = float3(0, 0, 0);
-static float3 projectorTarget = float3(0, 0, 0);
-
-const char * listbox_items[] = { "GL_ZERO", "GL_ONE", "GL_SRC_COLOR", "GL_ONE_MINUS_SRC_COLOR", "GL_DST_COLOR", "GL_ONE_MINUS_DST_COLOR", "GL_SRC_ALPHA", "GL_DST_ALPHA", "GL_ONE_MINUS_DST_ALPHA" };
-std::vector<int> blendModes = { GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA };
-
-static int src_blendmode = 0;
-static int dst_blendmode = 6;
-
-tinygizmo::rigid_transform terrainTransform;
-
 void shader_workbench::on_draw()
 {
     glfwMakeContextCurrent(window);
@@ -173,29 +79,38 @@ void shader_workbench::on_draw()
 
     int width, height;
     glfwGetWindowSize(window, &width, &height);
-    glViewport(0, 0, width, height);
 
-    gpuTimer.start();
-
-    /*
-    Blend SrcAlpha OneMinusSrcAlpha     // Alpha blending
-    Blend One One                       // Additive
-    Blend OneMinusDstColor One          // Soft Additive
-    Blend DstColor Zero                 // Multiplicative
-    Blend DstColor SrcColor             // 2x Multiplicative
-    */
-
-    // GL_ZERO, GL_SRC_ALPHA
     const float4x4 projectionMatrix = cam.get_projection_matrix(float(width) / float(height));
     const float4x4 viewMatrix = cam.get_view_matrix();
     const float4x4 viewProjectionMatrix = mul(projectionMatrix, viewMatrix);
+
     if (gizmo) gizmo->update(cam, float2(width, height));
 
-    fullscreenQuad = fullscreen_quad_extra(projectionMatrix, viewMatrix);
+    //tinygizmo::transform_gizmo("root", gizmo->gizmo_ctx, root_transform);
+    //tinygizmo::transform_gizmo("joint", gizmo->gizmo_ctx, joint_transform);
+    //tinygizmo::transform_gizmo("end", gizmo->gizmo_ctx, end_transform);
+    tinygizmo::transform_gizmo("target", gizmo->gizmo_ctx, target_transform);
 
-    tinygizmo::transform_gizmo("terrain", gizmo->gizmo_ctx, terrainTransform);
-    //float4x4 terrainModelMatrix = make_translation_matrix({ -8, 0, -8 });
-    float4x4 terrainModelMatrix = reinterpret_cast<const float4x4 &>(terrainTransform.matrix());
+    const float3 rootPosition = reinterpret_cast<const float3 &>(root_transform.position);
+    const float3 jointPosition = reinterpret_cast<const float3 &>(joint_transform.position);
+    const float3 endPosition = reinterpret_cast<const float3 &>(end_transform.position);
+
+    const float3 jointTarget = float3(0, 0, 0);
+    const float3 effectorPosition = reinterpret_cast<const float3 &>(target_transform.position);
+
+    float3 outJointPosition;
+    float3 outEndPosition;
+
+    SolveTwoBoneIK(rootPosition, jointPosition, endPosition, jointTarget, effectorPosition, outJointPosition, outEndPosition, false, 1.f, 1.f);
+
+    float4x4 rootMatrix = reinterpret_cast<const float4x4 &>(root_transform.matrix());
+    float4x4 jointMatrix = reinterpret_cast<const float4x4 &>(joint_transform.matrix());
+    float4x4 endMatrix = reinterpret_cast<const float4x4 &>(end_transform.matrix());
+
+    float4x4 outJointMatrix = mul(make_translation_matrix(outJointPosition), make_scaling_matrix(0.5));
+    float4x4 outEffectorMatrix = mul(make_translation_matrix(outEndPosition), make_scaling_matrix(0.5));
+
+    gpuTimer.start();
 
     // Main Scene
     {
@@ -204,113 +119,28 @@ void shader_workbench::on_draw()
 
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        if (renderColor)
-        {
-            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-            triplanarTexture->bind();
-            triplanarTexture->uniform("u_viewProj", viewProjectionMatrix);
-            triplanarTexture->uniform("u_modelMatrix", terrainModelMatrix);
-            triplanarTexture->uniform("u_modelMatrixIT", inv(transpose(terrainModelMatrix)));
-            triplanarTexture->texture("s_diffuseTextureA", 0, rustyTexture, GL_TEXTURE_2D);
-            triplanarTexture->texture("s_diffuseTextureB", 1, topTexture, GL_TEXTURE_2D);
-            triplanarTexture->uniform("u_scale", scale);
-            terrainMesh.draw_elements();
-            triplanarTexture->unbind();
-        }
-
-        if (renderProjective)
-        {
-            //glDisable(GL_DEPTH_TEST);
-            glEnable(GL_POLYGON_OFFSET_FILL);
-            glPolygonOffset(-1.0, -1.0);
-            glBlendFunc(blendModes[src_blendmode], blendModes[dst_blendmode]);
-
-            projector.projectorMultiplyShader->bind();
-
-            float4x4 projectorViewProj = projector.get_view_projection_matrix();
-
-            projector.projectorMultiplyShader->uniform("u_time", elapsedTime);
-            projector.projectorMultiplyShader->uniform("u_eye", cam.get_eye_point());
-            projector.projectorMultiplyShader->uniform("u_viewProj", viewProjectionMatrix);
-            projector.projectorMultiplyShader->uniform("u_projector", projectorViewProj);
-            projector.projectorMultiplyShader->uniform("u_modelMatrix", terrainModelMatrix);
-            projector.projectorMultiplyShader->uniform("u_modelMatrixIT", inv(transpose(terrainModelMatrix)));
-            projector.projectorMultiplyShader->texture("s_cookieTex", 0, *projector.cookieTexture, GL_TEXTURE_2D);
-            projector.projectorMultiplyShader->texture("s_gradientTex", 1, *projector.gradientTexture, GL_TEXTURE_2D);
-
-            terrainMesh.draw_elements();
-
-            projector.projectorMultiplyShader->unbind();
-
-            glDisable(GL_POLYGON_OFFSET_FILL);
-        }
-
-        glDisable(GL_BLEND);
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        normalDebug->bind();
+        normalDebug->uniform("u_viewProj", viewProjectionMatrix);
+
+        // Some debug models
+        for (const auto & model : { rootMatrix, jointMatrix, endMatrix, outJointMatrix, outEffectorMatrix })
+        {
+            normalDebug->uniform("u_modelMatrix", model);
+            normalDebug->uniform("u_modelMatrixIT", inv(transpose(model)));
+            sphere_mesh.draw_elements();
+        }
+
+        normalDebug->unbind();
     }
-
-    /*
-    // Screenspace Effect
-    {
-        glDisable(GL_DEPTH_TEST);
-
-        terrainScan->bind();
-
-        terrainScan->uniform("u_time", elapsedTime);
-        terrainScan->uniform("u_eye", cam.get_eye_point());
-
-        terrainScan->uniform("u_inverseViewProjection", inverse(viewProjectionMatrix));
-
-        terrainScan->uniform("u_scanDistance", scanDistance);
-        terrainScan->uniform("u_scanWidth", scanWidth);
-        terrainScan->uniform("u_leadSharp", leadSharp);
-        terrainScan->uniform("u_leadColor", leadColor);
-        terrainScan->uniform("u_midColor", midColor);
-        terrainScan->uniform("u_trailColor", trailColor);
-        terrainScan->uniform("u_hbarColor", hbarColor);
-
-        terrainScan->texture("s_colorTex", 0, sceneColorTexture, GL_TEXTURE_2D);
-        terrainScan->texture("s_depthTex", 1, sceneDepthTexture, GL_TEXTURE_2D);
-
-        fullscreenQuad.draw_elements();
-
-        terrainScan->unbind();
-    }
-    */
 
     gpuTimer.stop();
 
     igm->begin_frame();
-
-    if (ImGui::SliderFloat3("Projector Eye", &projectorEye.x, -5.f, 5.f))
-    {
-        projector.pose = look_at_pose_rh(projectorEye, projectorTarget);
-    }
-    if (ImGui::SliderFloat3("Projector Target", &projectorTarget.x, -5.f, 5.f))
-    {
-        projector.pose = look_at_pose_rh(projectorEye, projectorTarget);
-    }
-
-    ImGui::Text("Render Time %f ms", gpuTimer.elapsed_ms());
-    ImGui::Checkbox("Render Color", &renderColor);
-    ImGui::Checkbox("Render Projective", &renderProjective);
-    ImGui::ListBox("Src Blendmode", &src_blendmode, listbox_items, IM_ARRAYSIZE(listbox_items), 9);
-    ImGui::ListBox("DestBlendmode", &dst_blendmode, listbox_items, IM_ARRAYSIZE(listbox_items), 9);
-
-    /*
-    ImGui::SliderFloat3("Triplanar Scale", &scale.x, 0.0f, 1.f);
-    ImGui::SliderFloat("Scan Distance", &scanDistance, 0.1f, 10.f);
-    ImGui::SliderFloat("Scan Width", &scanWidth, 0.1f, 10.f);
-    ImGui::SliderFloat("Lead Sharp", &leadSharp, 0.1f, 10.f);
-    ImGui::SliderFloat4("Lead Color", &leadColor.x, 0.0f, 1.f);
-    ImGui::SliderFloat4("Mid Color", &midColor.x, 0.0f, 1.f);
-    ImGui::SliderFloat4("Trail Color", &trailColor.x, 0.0f, 1.f);
-    ImGui::SliderFloat4("hbarColor Color", &hbarColor.x, 0.0f, 1.f);
-    */
-
+    // ... 
     igm->end_frame();
+
     if (gizmo) gizmo->draw();
 
     gl_check_error(__FILE__, __LINE__);
--- shader-workbench/workbench.hpp
+++ shader-workbench/workbench.hpp
@@ -1,45 +1,169 @@
 #include "index.hpp"
 #include "gl-gizmo.hpp"
-#include "holo-scan-effect.hpp"
-#include "terrain-scan-effect.hpp"
-#include "fade-to-skybox.hpp"
-#include "cheap-subsurface-scattering.hpp"
-#include "area-light-ltc.hpp"
-#include "lab-teleportation-sphere.hpp"
-#include "triplanar-terrain.hpp"
 
-struct gl_material_projector
+#define KINDA_SMALL_NUMBER 0.001f
+
+inline float square(const float value) { return value * value; }
+
+// Find good arbitrary axis vectors to represent U and V axes of a plane, using this vector as the normal of the plane.
+void find_best_axis_vectors(const float3 & vector, float3 & Axis1, float3 & Axis2)
 {
-    std::shared_ptr<GlShader> projectorLightShader;
-    std::shared_ptr<GlShader> projectorMultiplyShader;
+    const float3 N = abs(vector);
 
-    std::shared_ptr<GlTexture2D> cookieTexture;
-    std::shared_ptr<GlTexture2D> gradientTexture;
+    if (N.z > N.x && N.z > N.y) Axis1 = float3(1, 0, 0);
+    else Axis1 = float3(0, 0, 1);
 
-    enum projector_material
+    Axis1 = safe_normalize(Axis1 - vector * dot(Axis1,vector));
+    Axis2 = cross(Axis1, vector);
+}
+
+//SolveTwoBoneIK(false, 1.f, 1.f);
+
+// FaceJointTarget
+
+/**
+* Two Bone IK
+*
+* This handles two bone chain link excluding root bone. This will solve the solution for joint/end when given root, joint, end position (root->joint->end in the hierarchy)
+* based on effector, joint target location
+* This only solves for location, if you want to rotate them to face target, this doesn't do it for you
+*
+* @param	RootPos				Current Root position
+* @param	JointPos			Current Joint position
+* @param	EndPos				Current End position
+* @param	JointTarget			Joint Target position (where joint is facing while creating plane between joint pos, joint target, root pos(rotate-plane ik))
+* @param	Effector			Effector position (target position)
+* @param	OutJointPos			(out) adjusted joint pos
+* @param	OutEndPos			(out) adjusted end pos
+* @param	bAllowStretching	whether or not to allow stretching or not
+* @param	StartStretchRatio	When should it start stretch -i.e. 1 means its own length without any stretch
+* @param	MaxStretchScale		How much it can stretch to in ratio
+*/
+void SolveTwoBoneIK(
+    const float3 & RootPos,
+    const float3 & JointPos,
+    const float3 & EndPos,
+    const float3 & JointTarget,
+    const float3 & Effector,
+    float3 & OutJointPos,
+    float3 & OutEndPos,
+    bool bAllowStretching,
+    float StartStretchRatio,
+    float MaxStretchScale)
+{
+
+    float LowerLimbLength = length(EndPos - JointPos);
+    float UpperLimbLength = length(JointPos - RootPos);
+
+    // This is our reach goal.
+    float3 DesiredPos = Effector;
+    float3 DesiredDelta = DesiredPos - RootPos;
+    float DesiredLength = length(DesiredDelta);
+
+    // Find lengths of upper and lower limb in the ref skeleton.
+    // Use actual sizes instead of ref skeleton, so we take into account translation and scaling from other bone controllers.
+    float MaxLimbLength = LowerLimbLength + UpperLimbLength;
+
+    // Check to handle case where DesiredPos is the same as RootPos.
+    float3	DesiredDir;
+    if (DesiredLength < (float) KINDA_SMALL_NUMBER)
     {
-        LIGHT,
-        MULTIPLY
-    };
-
-    projector_material type{ projector_material::MULTIPLY };
-
-    bool isOrthographic{ false };
-
-    Pose pose;
-
-    float4x4 get_view_projection_matrix()
+        DesiredLength = (float)KINDA_SMALL_NUMBER;
+        DesiredDir = float3(1, 0, 0);
+    }
+    else
     {
-        if (isOrthographic)
-        {
-            const float halfSize = 1.0 * 0.5f;
-            return mul(make_orthographic_matrix(-halfSize, halfSize, -halfSize, halfSize, -halfSize, halfSize), make_view_matrix_from_pose(pose));
-        }
-        
-        return mul(make_perspective_matrix(to_radians(75.f), 1.0f, 0.05f, 32.f), make_view_matrix_from_pose(pose));
+        DesiredDir = safe_normalize(DesiredDelta);
     }
 
-};
+    // Get joint target (used for defining plane that joint should be in).
+    float3 JointTargetDelta = JointTarget - RootPos;
+    const float JointTargetLengthSqr = length2(JointTargetDelta);
+
+    // Same check as above, to cover case when JointTarget position is the same as RootPos.
+    float3 JointPlaneNormal, JointBendDir;
+    if (JointTargetLengthSqr < square((float)KINDA_SMALL_NUMBER))
+    {
+        JointBendDir = float3(0, 1, 0);
+        JointPlaneNormal = float3(0, 0, 1);
+    }
+    else
+    {
+        JointPlaneNormal = cross(DesiredDir, JointTargetDelta);
+
+        // If we are trying to point the limb in the same direction that we are supposed to displace the joint in, 
+        // we have to just pick 2 random vector perp to DesiredDir and each other.
+        if (length2(JointPlaneNormal) < square((float)KINDA_SMALL_NUMBER))
+        {
+            find_best_axis_vectors(DesiredDir, JointPlaneNormal, JointBendDir);
+        }
+        else
+        {
+            JointPlaneNormal = normalize(JointPlaneNormal);
+
+            // Find the final member of the reference frame by removing any component of JointTargetDelta along DesiredDir.
+            // This should never leave a zero vector, because we've checked DesiredDir and JointTargetDelta are not parallel.
+            JointBendDir = normalize(JointTargetDelta - (dot(JointTargetDelta, DesiredDir) * DesiredDir));
+        }
+    }
+
+    if (bAllowStretching)
+    {
+        const float ScaleRange = MaxStretchScale - StartStretchRatio;
+        if (ScaleRange > KINDA_SMALL_NUMBER && MaxLimbLength > KINDA_SMALL_NUMBER)
+        {
+            const float ReachRatio = DesiredLength / MaxLimbLength;
+            const float ScalingFactor = (MaxStretchScale - 1.f) * clamp((ReachRatio - StartStretchRatio) / ScaleRange, 0.f, 1.f);
+            if (ScalingFactor > KINDA_SMALL_NUMBER)
+            {
+                LowerLimbLength *= (1.f + ScalingFactor);
+                UpperLimbLength *= (1.f + ScalingFactor);
+                MaxLimbLength   *= (1.f + ScalingFactor);
+            }
+        }
+    }
+
+    OutEndPos = DesiredPos;
+    OutJointPos = JointPos;
+
+    // If we are trying to reach a goal beyond the length of the limb, clamp it to something solvable and extend limb fully.
+    if (DesiredLength >= MaxLimbLength)
+    {
+        OutEndPos = RootPos + (MaxLimbLength * DesiredDir);
+        OutJointPos = RootPos + (UpperLimbLength * DesiredDir);
+    }
+    else
+    {
+        // So we have a triangle we know the side lengths of. We can work out the angle between DesiredDir and the direction of the upper limb
+        // using the sin rule:
+        const float TwoAB = 2.f * UpperLimbLength * DesiredLength;
+
+        const float CosAngle = (TwoAB != 0.f) ? ((UpperLimbLength*UpperLimbLength) + (DesiredLength*DesiredLength) - (LowerLimbLength*LowerLimbLength)) / TwoAB : 0.f;
+
+        // If CosAngle is less than 0, the upper arm actually points the opposite way to DesiredDir, so we handle that.
+        const bool bReverseUpperBone = (CosAngle < 0.f);
+
+        // Angle between upper limb and DesiredDir
+        // ACos clamps internally so we dont need to worry about out-of-range values here.
+        const float Angle = std::acos(CosAngle);
+
+        // Now we calculate the distance of the joint from the root -> effector line.
+        // This forms a right-angle triangle, with the upper limb as the hypotenuse.
+        const float JointLineDist = UpperLimbLength * std::sin(Angle);
+
+        // And the final side of that triangle - distance along DesiredDir of perpendicular.
+        // ProjJointDistSqr can't be neg, because JointLineDist must be <= UpperLimbLength because appSin(Angle) is <= 1.
+        const float ProjJointDistSqr = (UpperLimbLength*UpperLimbLength) - (JointLineDist*JointLineDist);
+
+        // although this shouldn't be ever negative, sometimes Xbox release produces -0.f, causing ProjJointDist to be NaN
+        // so now I branch it. 						
+        float ProjJointDist = (ProjJointDistSqr > 0.f) ? std::sqrt(ProjJointDistSqr) : 0.f;
+        if (bReverseUpperBone) ProjJointDist *= -1.f;
+
+        // So now we can work out where to put the joint!
+        OutJointPos = RootPos + (ProjJointDist * DesiredDir) + (JointLineDist * JointBendDir);
+    }
+}
 
 struct shader_workbench : public GLFWApp
 {
@@ -48,30 +172,14 @@ struct shader_workbench : public GLFWApp
     ShaderMonitor shaderMonitor{ "../assets/" };
     std::unique_ptr<gui::ImGuiManager> igm;
     GlGpuTimer gpuTimer;
+
     std::unique_ptr<GlGizmo> gizmo;
-
-    GlTexture2D rustyTexture, topTexture;
-    GlTexture2D sceneColorTexture;
-    GlTexture2D sceneDepthTexture;
-    GlFramebuffer sceneFramebuffer;
-
-    gl_material_projector projector;
 
     float elapsedTime{ 0 };
 
-    float scanDistance{ 1.0f };
-    float scanWidth{ 5.0f };
-    float leadSharp{ 8.0f };
-    float4 leadColor{ 0.8f, 0.6f, 0.3f, 0};
-    float4 midColor{ 0.975f, 0.78f, 0.366f, 0 };
-    float4 trailColor{ 1.f, 0.83f, 1, 0 };
-    float4 hbarColor{ 0.05, 0.05, 0.05, 0 };
-
-    std::shared_ptr<GlShader> terrainScan;
-    std::shared_ptr<GlShader> triplanarTexture;
     std::shared_ptr<GlShader> normalDebug;
 
-    GlMesh terrainMesh, fullscreenQuad;
+    GlMesh sphere_mesh;
 
     shader_workbench();
     ~shader_workbench();
