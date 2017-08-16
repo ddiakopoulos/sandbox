fca9ba6a5cca45beae0cea75c95e53f43e9fe60c
dimitri <ddiakopoulos@gmail.com>
2017-08-16 09:41:35 -0700

    fixup the terrain/water rendering example

--- examples/terrain_app.hpp
+++ examples/terrain_app.hpp
@@ -14,17 +14,14 @@ struct ExperimentalApp : public GLFWApp
     std::unique_ptr<GlShader> waterShader;
     
     GlFramebuffer reflectionFramebuffer;
-    GlTexture sceneColorTexture;
+    GlTexture2D sceneColorTexture;
     
     GlFramebuffer depthFramebuffer;
-    GlTexture sceneDepthTexture;
+    GlTexture2D sceneDepthTexture;
     
-    Renderable waterMesh;
-    Renderable terrainMesh;
-    Renderable icosahedron;
-    
-    std::unique_ptr<GLTextureView> colorTextureView;
-    std::unique_ptr<GLTextureView> depthTextureView;
+    GlMesh waterMesh;
+    GlMesh terrainMesh;
+    GlMesh icosahedronMesh;
     
     const float clipPlaneOffset = 0.075f;
     
@@ -32,15 +29,13 @@ struct ExperimentalApp : public GLFWApp
     int yIndex = 0;
     float4x4 terrainTranslationMat = make_translation_matrix({-16, static_cast<float>(yIndex), -16});
     
-    //UWidget rootWidget;
-    
     float appTime = 0;
     float rotationAngle = 0.0f;
     
     std::random_device rd;
     std::mt19937 gen;
     
-    ExperimentalApp() : GLFWApp(940, 720, "Terrain & Water Scene")
+    ExperimentalApp() : GLFWApp(1280, 720, "Terrain & Water Scene")
     {
         gen = std::mt19937(rd());
         
@@ -48,39 +43,29 @@ struct ExperimentalApp : public GLFWApp
         glfwGetWindowSize(window, &width, &height);
         glViewport(0, 0, width, height);
         
+        camera.farclip = 96;
+        camera.look_at({0, 4, 12}, {0, 0.1f, 0});
+
         cameraController.set_camera(&camera);
-        
-        camera.look_at({0, 4, 12}, {0, 0, 0});
 
-        terrainShader.reset(new GlShader(read_file_text("assets/shaders/terrain_vert_debug.glsl"), read_file_text("assets/shaders/terrain_frag_debug.glsl")));
-        waterShader.reset(new GlShader(read_file_text("assets/shaders/water_vert.glsl"), read_file_text("assets/shaders/water_frag.glsl")));
+        terrainShader.reset(new GlShader(read_file_text("../assets/shaders/terrain_vert_debug.glsl"), read_file_text("../assets/shaders/terrain_frag_debug.glsl")));
+        waterShader.reset(new GlShader(read_file_text("../assets/shaders/water_vert.glsl"), read_file_text("../assets/shaders/water_frag.glsl")));
         
-        sceneColorTexture.load_data(width, height, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-        reflectionFramebuffer.attach(GL_COLOR_ATTACHMENT0, sceneColorTexture);
-        if (!reflectionFramebuffer.check_complete()) throw std::runtime_error("incomplete framebuffer");
+        sceneColorTexture.setup(width, height, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+        glNamedFramebufferTexture2DEXT(reflectionFramebuffer, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sceneColorTexture, 0);
+        reflectionFramebuffer.check_complete();
         
-        sceneDepthTexture.load_data(width, height, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
-        depthFramebuffer.attach(GL_DEPTH_ATTACHMENT, sceneDepthTexture);
-        if (!depthFramebuffer.check_complete()) throw std::runtime_error("incomplete framebuffer");
-        
+        sceneDepthTexture.setup(width, height, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+        glNamedFramebufferTexture2DEXT(depthFramebuffer, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, sceneDepthTexture, 0);
+        depthFramebuffer.check_complete();
+
         gl_check_error(__FILE__, __LINE__);
 
-        waterMesh = Renderable(make_plane(112.f, 112.f, 256, 256));
-        
-        terrainMesh = make_perlin_mesh(64, 64); //Renderable(make_cube());
-        
-        icosahedron = Renderable(make_icosahedron());
-        
-        colorTextureView.reset(new GLTextureView(sceneColorTexture.get_gl_handle()));
-        depthTextureView.reset(new GLTextureView(sceneDepthTexture.get_gl_handle()));
+        waterMesh = make_plane_mesh(112.f, 112.f, 256, 256);
+        terrainMesh = make_mesh_from_geometry(make_perlin_mesh(64, 64));
+        icosahedronMesh = make_icosahedron_mesh();
         
         gl_check_error(__FILE__, __LINE__);
-        
-        // Set up the UI
-        //rootWidget.bounds = {0, 0, (float) width, (float) height};
-       // rootWidget.add_child( {{0,+10},{0,+10},{0.25,0},{0.25,0}}, std::make_shared<UWidget>()); // for colorTexture
-       // rootWidget.add_child( {{.25,+10},{0, +10},{0.50, -10},{0.25,0}}, std::make_shared<UWidget>()); // for depthTexture
-       // rootWidget.layout();
     }
     
     std::vector<float> make_radial_mask(int size, float heightScale = 1.0)
@@ -200,7 +185,7 @@ struct ExperimentalApp : public GLFWApp
             terrainShader->uniform("u_modelView", modelViewMat);
             terrainShader->uniform("u_modelMatrixIT", get_rotation_submatrix(inv(transpose(modelViewMat))));
             terrainShader->uniform("u_surfaceColor", float3(95.f / 255.f, 189.f / 255.f, 192.f / 255.f));
-            terrainMesh.draw();
+            terrainMesh.draw_elements();
         }
         
         {
@@ -214,7 +199,7 @@ struct ExperimentalApp : public GLFWApp
             terrainShader->uniform("u_modelView", modelViewMat);
             terrainShader->uniform("u_modelMatrixIT", get_rotation_submatrix(inv(transpose(modelViewMat))));
             terrainShader->uniform("u_surfaceColor", float3(189.f / 255.f, 94.f / 255.f, 188.f / 255.f));
-            icosahedron.draw();
+            icosahedronMesh.draw_elements();
         }
         
         terrainShader->unbind();
@@ -222,16 +207,6 @@ struct ExperimentalApp : public GLFWApp
         glDisable(GL_BLEND);
         
         gl_check_error(__FILE__, __LINE__);
-    }
-    
-    void draw_ui()
-    {
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-        
-        //colorTextureView->draw(rootWidget.children[0]->bounds, int2(width, height));
-        //depthTextureView->draw(rootWidget.children[1]->bounds, int2(width, height));
     }
     
     // Given position/normal of the plane, calculates plane in camera space.
@@ -246,108 +221,113 @@ struct ExperimentalApp : public GLFWApp
     
     void on_draw() override
     {
-        glfwMakeContextCurrent(window);
-        
         glEnable(GL_CULL_FACE);
         glEnable(GL_DEPTH_TEST);
         glDisable(GL_SCISSOR_TEST);
 
         int width, height;
         glfwGetWindowSize(window, &width, &height);
-        glViewport(0, 0, width, height);
-        
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glClearColor(0.f, 0.0f, 0.0f, 1.0f);
-        
-        float4x4 viewProj = mul(camera.get_projection_matrix((float) width / (float) height), camera.get_view_matrix());
-        
-        skydome.render(viewProj, camera.get_eye_point(), camera.farClip);
-        
+
+        const float4x4 viewProj = mul(camera.get_projection_matrix((float)width / (float)height), camera.get_view_matrix());
+
+        skydome.render(viewProj, camera.get_eye_point(), camera.farclip);
+
         {
             // Wind in reverse order for reflection
             glFrontFace(GL_CW);
-            
-            reflectionFramebuffer.bind_to_draw();
+
+            glBindFramebuffer(GL_FRAMEBUFFER, reflectionFramebuffer);
+            glViewport(0, 0, width, height);
+
             glClear(GL_COLOR_BUFFER_BIT);
             glClearColor(0.f, 0.0f, 0.0f, 1.0f);
-            
+
             // Reflect camera around reflection plane
             float3 normal = float3(0, 1, 0);
-            float3 pos = {0, 0, 0}; // Location of object... here, the terrain
+            float3 pos = { 0, 0, 0 }; // Location of object... here, the terrain
             float4 reflectionPlane = float4(normal.x, normal.y, normal.z, 0.f);
 
             float4x4 model = mul(Identity4x4, terrainTranslationMat);
-            
-			// Take position, transform into world space with model, reflect about a world space reflection plane, then take it into the view space of the camera
-			// ViewProj takes the camera local coordinate into screen coordinates (3D to the 2D + Z)
-			// gl_position = proj * view * refl * model * position;
+
+            // Take position, transform into world space with model, reflect about a world space reflection plane, then take it into the view space of the camera
+            // ViewProj takes the camera local coordinate into screen coordinates (3D to the 2D + Z)
+            // gl_position = proj * view * refl * model * position;
 
             terrainShader->bind();
             terrainShader->uniform("u_viewProj", mul(viewProj, make_reflection_matrix(reflectionPlane)));
-			terrainShader->uniform("u_model", model);
-			//terrainShader->uniform("u_modelMatrixIT", get_rotation_submatrix(inv(transpose(modelViewMat))));
-            terrainShader->uniform("u_eyePosition", camera.get_eye_point());            
+            terrainShader->uniform("u_model", model);
+            terrainShader->uniform("u_eyePosition", camera.get_eye_point());
             terrainShader->uniform("u_clipPlane", reflectionPlane);
             terrainShader->uniform("u_lightPosition", float3(0.0, 10.0, 0.0));
             terrainShader->uniform("u_surfaceColor", float3(1, 1, 1));
-            terrainMesh.draw();
-            
+            terrainMesh.draw_elements();
+
             terrainShader->unbind();
-            
-            reflectionFramebuffer.unbind();
 
             // Pop reverse winding
             glFrontFace(GL_CCW);
         }
-        
+
         {
-            depthFramebuffer.bind_to_draw();
+            glBindFramebuffer(GL_FRAMEBUFFER, depthFramebuffer);
+            glViewport(0, 0, width, height);
+
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            draw_terrain();
-            gl_check_error(__FILE__, __LINE__);
-            depthFramebuffer.unbind();
-        }
-        
-         draw_terrain();
-        
-        // Draw Water
-        {
-            // Gives it a bit of a wispy look....
-            // glEnable(GL_BLEND);
-            // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-            float4x4 model = make_rotation_matrix({1, 0, 0}, -ANVIL_PI / 2);
-            auto mvp = mul(camera.get_projection_matrix((float) width / (float) height), camera.get_view_matrix(), model);
-            float4x4 modelViewMat = mul(camera.get_view_matrix(), model);
-            
-            waterShader->bind();
-            
-            waterShader->uniform("u_mvp", mvp);
-            waterShader->uniform("u_time", appTime);
-            waterShader->uniform("u_yWaterPlane", yWaterPlane);
-            waterShader->uniform("u_eyePosition", camera.get_eye_point());
-            waterShader->uniform("u_modelView", modelViewMat);
-            waterShader->uniform("u_modelMatrixIT", get_rotation_submatrix(inv(transpose(modelViewMat))));
-            waterShader->uniform("u_resolution", float2(width, height));
-            
-            waterShader->uniform("u_clipPlane", {0, 1, 0, 0});
-            
-            waterShader->texture("u_reflectionTexture", 0, sceneColorTexture.get_gl_handle(), GL_TEXTURE_2D);
-            waterShader->texture("u_depthTexture", 1, sceneDepthTexture.get_gl_handle(), GL_TEXTURE_2D);
-            
-            waterShader->uniform("u_near", camera.nearClip);
-            waterShader->uniform("u_far", camera.farClip);
-            waterShader->uniform("u_lightPosition", float3(0.0, 10.0, 0.0));
-            
-            waterMesh.draw();
-            waterShader->unbind();
-            
-            //glDisable(GL_BLEND);
+            draw_terrain();
+
+            gl_check_error(__FILE__, __LINE__);
         }
-        
-        draw_ui();
-        
+
+        // Output to default framebuffer
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+            glViewport(0, 0, width, height);
+
+            draw_terrain();
+
+            // Draw Water
+            {
+                // Gives it a bit of a wispy look....
+                //glEnable(GL_BLEND);
+                //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+                float4x4 model = make_rotation_matrix({ 1, 0, 0 }, -ANVIL_PI / 2);
+
+                auto mvp = mul(camera.get_projection_matrix((float)width / (float)height), camera.get_view_matrix(), model);
+                float4x4 modelViewMat = mul(camera.get_view_matrix(), model);
+
+                waterShader->bind();
+
+                waterShader->uniform("u_mvp", mvp);
+                waterShader->uniform("u_time", appTime);
+                waterShader->uniform("u_yWaterPlane", yWaterPlane);
+                waterShader->uniform("u_eyePosition", camera.get_eye_point());
+                waterShader->uniform("u_modelView", modelViewMat);
+                waterShader->uniform("u_modelMatrixIT", get_rotation_submatrix(inv(transpose(model))));
+                waterShader->uniform("u_resolution", float2(width, height));
+
+                waterShader->uniform("u_clipPlane", { 0, 1, 0, 0 });
+
+                waterShader->texture("u_reflectionTexture", 0, sceneColorTexture.id(), GL_TEXTURE_2D);
+                waterShader->texture("u_depthTexture", 1, sceneDepthTexture.id(), GL_TEXTURE_2D);
+
+                waterShader->uniform("u_near", camera.nearclip);
+                waterShader->uniform("u_far", camera.farclip);
+                waterShader->uniform("u_lightPosition", float3(0.0, 10.0, 0.0));
+
+                waterMesh.draw_elements();
+                waterShader->unbind();
+
+                //glDisable(GL_BLEND);
+            }
+
+        }
+
         gl_check_error(__FILE__, __LINE__);
         
         glfwSwapBuffers(window);
