fea902a531442eaff662056331bbe2bbb87d8401
dimitri <ddiakopoulos@gmail.com>
2017-08-25 09:02:33 -0700

    remove camera_app

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,17 +8,16 @@
 using namespace avl;
 
 #include "examples/empty_app.hpp"
-//#include "examples/textured_model_app.hpp"
-//#include "examples/camera_app.hpp"
-//#include "examples/meshline_app.hpp"
-//#include "examples/reaction_app.hpp"
-//#include "examples/decal_app.hpp"
-//#include "examples/instance_app.hpp"
-//#include "examples/simplex_noise_app.hpp"
-//#include "examples/geometric_algo_dev.hpp"
 //#include "examples/asset_test.hpp"
+//#include "examples/decal_app.hpp"
+//#include "examples/geometric_algo_dev.hpp"
+//#include "examples/instance_app.hpp"
+//#include "examples/meshline_app.hpp"
 //#include "examples/octree_test_app.hpp"
+//#include "examples/reaction_app.hpp"
+//#include "examples/simplex_noise_app.hpp"
 //#include "examples/terrain_app.hpp"
+//#include "examples/textured_model_app.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "third_party/stb/stb_image.h"
