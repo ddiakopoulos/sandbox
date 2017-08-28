6886722ca041b826635e610bd417f755fb5d7ffb
dimitri <ddiakopoulos@gmail.com>
2017-08-28 13:04:24 -0700

    test with multiple includes

--- gl/gl-shader-monitor.hpp
+++ gl/gl-shader-monitor.hpp
@@ -13,7 +13,7 @@
 namespace avl
 {
 
-    inline std::string preprocess_includes(const std::string & source, const std::string & includeSearchPath, int depth)
+    inline std::string preprocess_includes(const std::string & source, const std::string & includeSearchPath, std::vector<std::string> & includes, int depth)
     {
         if (depth > 4) throw std::runtime_error("exceeded max include recursion depth");
 
@@ -36,7 +36,8 @@ namespace avl
 
                 if (!includeFile.empty())
                 {
-                    output << preprocess_includes(includeString, includeSearchPath, depth++) << std::endl;
+                    includes.push_back(includeSearchPath + "/" + includeFile);
+                    output << preprocess_includes(includeString, includeSearchPath, includes, depth++) << std::endl;
                 }
             }
             else
@@ -76,7 +77,8 @@ namespace avl
         const std::string & vertexShader, 
         const std::string & fragmentShader, 
         const std::string & includeSearchPath, 
-        const std::vector<std::string> & defines)
+        const std::vector<std::string> & defines, 
+        std::vector<std::string> & includes)
     {
         std::stringstream vertex;
         std::stringstream fragment;
@@ -93,8 +95,8 @@ namespace avl
         std::unique_ptr<GlShader> result;
 
         result.reset(new GlShader(
-            preprocess_version(preprocess_includes(vertex.str(), includeSearchPath, 0)),
-            preprocess_version(preprocess_includes(fragment.str(), includeSearchPath, 0))));
+            preprocess_version(preprocess_includes(vertex.str(), includeSearchPath, includes, 0)),
+            preprocess_version(preprocess_includes(fragment.str(), includeSearchPath, includes, 0))));
 
         return result;
     }
@@ -111,10 +113,18 @@ namespace avl
             std::string geomPath;
             std::string includePath;
             std::vector<std::string> defines;
+            std::vector<std::string> includes;
             bool shouldRecompile = false;
             std::string errorStr;
-            ShaderAsset(std::shared_ptr<GlShader> program, const std::string & v, const std::string & f, const std::string & g = "", const std::string & inc = "", const std::vector<std::string> & defines = {})
-                : program(program), vertexPath(v), fragmentPath(f), geomPath(g), includePath(inc), defines(defines) { };
+            ShaderAsset(
+                std::shared_ptr<GlShader> program, 
+                const std::string & v, 
+                const std::string & f, 
+                const std::string & g = "", 
+                const std::string & inc = "", 
+                const std::vector<std::string> & defines = {},
+                const std::vector<std::string> & includePaths = {})
+                : program(program), vertexPath(v), fragmentPath(f), geomPath(g), includePath(inc), defines(defines), includes(includePaths) { };
         };
 
         struct UpdateListener : public efsw::FileWatchListener
@@ -146,10 +156,23 @@ namespace avl
             {
                 for (auto & shader : shaders)
                 {
+                    // Recompile if any one of the shader stages have changed
                     if (get_filename_with_extension(filename) == get_filename_with_extension(shader->vertexPath) || 
-                        get_filename_with_extension(filename) == get_filename_with_extension(shader->fragmentPath))
+                        get_filename_with_extension(filename) == get_filename_with_extension(shader->fragmentPath) || 
+                        get_filename_with_extension(filename) == get_filename_with_extension(shader->geomPath))
                     {
                         shader->shouldRecompile = true;
+                    }
+
+                    // Each shader keeps a list of the files it includes. ShaderMonitor watches a base path,
+                    // so we should be able to recompile shaders depenent on common includes
+                    for (auto & includePath : shader->includes)
+                    {
+                        if (get_filename_with_extension(filename) == get_filename_with_extension(includePath))
+                        {
+                            shader->shouldRecompile = true;
+                            break;
+                        }
                     }
                 }
             };
@@ -185,9 +208,16 @@ namespace avl
             const std::string & includePath,
             const std::vector<std::string> & defines)
         {
-            auto src = preprocess(read_file_text(vertexShader), read_file_text(fragmentShader), includePath, defines);
+            // Preprocess this bundle
+            std::vector<std::string> includes;
+            auto src = preprocess(read_file_text(vertexShader), read_file_text(fragmentShader), includePath, defines, includes);
             std::shared_ptr<GlShader> watchedShader(std::move(src));
-            shaders.emplace_back(new ShaderAsset(watchedShader, vertexShader, fragmentShader, "", includePath, defines));
+            
+            // Add to watched assets
+            ShaderAsset * asset = new ShaderAsset(watchedShader, vertexShader, fragmentShader, "", includePath, defines, includes);
+            shaders.emplace_back(asset);
+
+            // Return shared_ptr to GlShader
             return watchedShader;
         }
 
@@ -218,12 +248,13 @@ namespace avl
                         // Needs to be run through the preprocessor
                         if (shader->defines.size() > 0 || shader->includePath.size() > 0)
                         {
-                            auto src = preprocess(read_file_text(shader->vertexPath), read_file_text(shader->fragmentPath), shader->includePath, shader->defines);
+                            std::vector<std::string> includes;
+                            auto src = preprocess(read_file_text(shader->vertexPath), read_file_text(shader->fragmentPath), shader->includePath, shader->defines, includes);
                             shader->program = std::move(src);
                         }
                         else
                         {
-                            //shader->program = GlShader(read_file_text(shader->vertexPath), read_file_text(shader->fragmentPath), read_file_text(shader->geomPath));
+                            shader->program = std::make_shared<GlShader>(read_file_text(shader->vertexPath), read_file_text(shader->fragmentPath), read_file_text(shader->geomPath));
                         }
 
                         if (shader->errorStr.length() > 0)
