62abd2aed3bff9406cf1e7860718d85c0aceb806
dimitri <ddiakopoulos@gmail.com>
2017-09-02 09:40:04 -0700

    experiment with the proposed gltf standard metallic-roughness workflow

--- assets/shaders/textured_pbr_frag.glsl
+++ assets/shaders/textured_pbr_frag.glsl
@@ -41,6 +41,22 @@ uniform sampler2DArray s_csmArray;
 
 out vec4 f_color;
 
+struct MaterialInfo
+{
+    float NdotL;                  // cos angle between normal and light direction
+    float NdotV;                  // cos angle between normal and view direction
+    float NdotH;                  // cos angle between normal and half vector
+    float LdotH;                  // cos angle between light direction and half vector
+    float VdotH;                  // cos angle between view direction and half vector
+    float perceptualRoughness;    // roughness value, as authored by the model creator (input to shader)
+    float metalness;              // metallic value at the surface
+    vec3 reflectance0;            // full reflectance color (normal incidence angle)
+    vec3 reflectance90;           // reflectance color at grazing angle
+    float alphaRoughness;         // roughness mapped to a more linear change in the roughness (proposed by [2])
+    vec3 diffuseColor;            // color contribution from diffuse lighting
+    vec3 specularColor;           // color contribution from specular lighting
+};
+
 // https://imdoingitwrong.wordpress.com/2011/01/31/light-attenuation/
 // https://imdoingitwrong.wordpress.com/2011/02/10/improved-light-attenuation/
 float point_light_attenuation(vec3 L, float lightRadius)
@@ -60,17 +76,6 @@ float point_light_attenuation(vec3 L, float lightRadius)
     return 1.0 / (f * f);
 }
 
-// http://the-witness.net/news/2012/02/seamless-cube-map-filtering/
-vec3 fix_cube_lookup(vec3 v, float cubeSize, float lod) 
-{
-    float M = max(max(abs(v.x), abs(v.y)), abs(v.z));
-    float scale = 1 - exp2(lod) / cubeSize;
-    if (abs(v.x) != M) v.x *= scale;
-    if (abs(v.y) != M) v.y *= scale;
-    if (abs(v.z) != M) v.z *= scale;
-    return v;
-}
-
 vec3 blend_normals(vec3 geometric, vec3 detail)
 {
     vec3 n1 = geometric;
@@ -79,84 +84,47 @@ vec3 blend_normals(vec3 geometric, vec3 detail)
     return normalize(n2.x*nBasis[0] + n2.y*nBasis[1] + n2.z*nBasis[2]);
 }
 
-// Blinn-Phong normal distribution, defined as 2d Gaussian distribution of microfacets' slopes. Sharp highlights.
-float NDF_Beckmann(float NoH, float alphaSqr)
+// The following equation models the Fresnel reflectance term of the spec equation (aka F())
+// Implementation of fresnel from [4], Equation 15
+vec3 specularReflection(MaterialInfo data)
 {
-    float NoH2 = NoH * NoH;
-    return 1.0 / (PI * alphaSqr * NoH2 * NoH2) * exp((NoH2 - 1.0) / (alphaSqr * NoH2));
+    return data.reflectance0 + (data.reflectance90 - data.reflectance0) * pow(clamp(1.0 - data.VdotH, 0.0, 1.0), 5.0);
 }
 
-// A popular model with a long tail. 
-float NDF_GGX(float NoH, float alphaSqr)
+// This calculates the specular geometric attenuation (aka G()),
+// where rougher material will reflect less light back to the viewer.
+// This implementation is based on [1] Equation 4, and we adopt their modifications to
+// alphaRoughness as input as originally proposed in [2].
+float geometricOcclusion(MaterialInfo data)
 {
-    return alphaSqr / (PI * pow(NoH * NoH * (alphaSqr - 1.0) + 1.0, 2.0)); 
-} 
+    float NdotL = data.NdotL;
+    float NdotV = data.NdotV;
+    float r = data.alphaRoughness;
 
-// Geometric attenuation/shadowing
-float schlick_smith_visibility(float NoL, float NoV, float alpha)
-{
-    float k = pow(0.8 + 0.5 * alpha, 2.0) / 2.0;
-    float GL = 1.0 / (NoL * (1.0 - k) + k);
-    float GV = 1.0 / (NoV * (1.0 - k) + k);
-    return GL * GV;
+    float attenuationL = 2.0 * NdotL / (NdotL + sqrt(r * r + (1.0 - r * r) * (NdotL * NdotL)));
+    float attenuationV = 2.0 * NdotV / (NdotV + sqrt(r * r + (1.0 - r * r) * (NdotV * NdotV)));
+    return attenuationL * attenuationV;
 }
 
-float fresnel_schlick(float ct, float F0)
+// The following equation(s) model the distribution of microfacet normals across the area being drawn (aka D())
+// Implementation from "Average Irregularity Representation of a Roughened Surface for Ray Reflection" by T. S. Trowbridge, and K. P. Reitz
+// Follows the distribution function recommended in the SIGGRAPH 2013 course notes from EPIC Games [1], Equation 3.
+float microfacetDistribution(MaterialInfo data)
 {
-    return F0 + (1.0 - F0) * pow(1.0 - ct, 5.0);
-}
-
-// https://www.unrealengine.com/blog/physically-based-shading-on-mobile
-vec3 env_brdf_approx(vec3 specularColor, float roughness, float NoV)
-{
-    const vec4 c0 = vec4(-1, -0.0275, -0.572, 0.022);
-    const vec4 c1 = vec4(1, 0.0425, 1.04, -0.04);
-    vec4 r = roughness * c0 + c1;
-    float a004 = min(r.x * r.x, exp2(-9.28 * NoV)) * r.x + r.y;
-    vec2 AB = vec2(-1.04, 1.04) * a004 + r.zw;
-    return specularColor * AB.x + AB.y;
-}
-
-float calculate_cook_torrance(vec3 N, vec3 V, vec3 L, float metalness, float roughness, float F0) 
-{
-    float alpha = max(u_specularLevel, roughness * roughness);
-    float alphaSqr = alpha * alpha;
-
-    vec3 H = normalize(V + L);
-
-    float NoL = saturate(dot(N, L));
-    float NoV = saturate(dot(N, V));
-    float NoH = saturate(dot(N, H));
-
-    // Normal distribution
-    float Di = NDF_GGX(NoH, alphaSqr);
-
-    // Fresnel term
-    float Fs = fresnel_schlick(NoV, F0);
-
-    // kS is equal to Fresnel
-    float kS = F0;
-
-    // for energy conservation, the diffuse and specular light can't
-    // be above 1.0 (unless the surface emits light); to preserve this
-    // relationship the diffuse component (kD) should equal 1.0 - kS.
-    float kD = 1.0 - kS;
-
-    // multiply kD by the inverse metalness such that only non-metals
-    // have diffuse lighting, or a linear blend if partly metal (pure metals have no diffuse light).
-    kD *= 1.0 - metalness;
-
-    // Geometry term is used for describing how much the microfacet is blocked by another microfacet
-    float Vs = schlick_smith_visibility(NoL, NoV, alpha);
-
-    return (Di * kD * Vs);
+    float roughnessSq = data.alphaRoughness * data.alphaRoughness;
+    float f = (data.NdotH * roughnessSq - data.NdotH) * data.NdotH + 1.0;
+    return roughnessSq / (PI * f * f);
 }
 
 void main()
 {   
     // Surface properties
     float roughness = texture(s_roughness, v_texcoord).r * clamp(u_roughness, u_specularLevel, 1.0);
-    float metallic = texture(s_metallic, v_texcoord).r * u_metallic;
+    float metallic = clamp(texture(s_metallic, v_texcoord).r * u_metallic,  0.0, 1.0);
+
+    // Roughness is authored as perceptual roughness; as is convention,
+    // convert to material roughness by squaring the perceptual roughness [2].
+    float alphaRoughness = roughness * roughness;
 
     // View direction
     vec3 V = normalize(u_eyePos.xyz - v_world_position);
@@ -164,70 +132,57 @@ void main()
     // Normal vector in worldspace
     vec3 N = blend_normals(v_normal, texture(s_normal, v_texcoord).xyz * 2.0 - 1.0);
 
-    vec3 albedo = sRGBToLinear(texture(s_albedo, v_texcoord).rgb, DEFAULT_GAMMA); //* (1 - metallic);
+    vec3 albedo = sRGBToLinear(texture(s_albedo, v_texcoord).rgb, DEFAULT_GAMMA); // * baseColor
 
-    // F0 (Fresnel reflection coefficient), otherwise known as specular reflectance. 
-    // 0.04 is the default for non-metals in UE.
-    // Calculate reflectance at normal incidence; if dialectric, use F0. 
-    // If metal, use albedo as F0 (metallic workflow) 
-    vec3 F0 = mix(vec3(u_specularLevel), albedo, u_metallic);
+    vec3 F0 = vec3(u_specularLevel);
+    vec3 diffuseColor = albedo * (vec3(1.0) - F0);
+    //diffuseColor *= 1.0 - metallic;
+    vec3 specularColor = mix(F0, albedo, metallic);
 
-    vec3 diffuseContrib = vec3(0);
-    vec3 specularContrib = vec3(0);
-    vec3 irradiance = vec3(1);
-    vec3 radiance = vec3(1);
+    // Compute reflectance.
+    float reflectance = max(max(specularColor.r, specularColor.g), specularColor.b);
 
-    float shadowTerm = calculate_csm_coefficient(s_csmArray, v_world_position, v_view_space_position, u_cascadesMatrix, u_cascadesPlane);
-    float shadowVisibility = 1.0 - shadowTerm;
+    // For typical incident reflectance range (between 4% to 100%) set the grazing reflectance to 100% for typical fresnel effect.
+    // For very low reflectance range on highly diffuse objects (below 4%), incrementally reduce grazing reflecance to 0%.
+    float reflectance90 = clamp(reflectance * 25.0, 0.0, 1.0);
+    vec3 specularEnvironmentR0 = specularColor.rgb;
+    vec3 specularEnvironmentR90 = vec3(1.0, 1.0, 1.0) * reflectance90;
 
-    // Compute directional light
-    {
-        vec3 L = normalize(u_directionalLight.direction);
-        float NoL = saturate(dot(N, L));
-
-        vec3 Ks = u_directionalLight.color * calculate_cook_torrance(N, V, L, metallic, roughness, F0.r);
-        vec3 Kd = (NoL * (albedo / PI));
-
-        diffuseContrib +=  Kd * u_directionalLight.amount;
-        specularContrib += (Ks / PI) * u_directionalLight.amount;
-    }
+    vec3 Lo = vec3(0);
 
     // Compute point lights
-    // https://seblagarde.wordpress.com/2012/01/08/pi-or-not-to-pi-in-game-lighting-equation/
     for (int i = 0; i < u_activePointLights; ++i)
     {
         vec3 L = normalize(u_pointLights[i].position - v_world_position); 
-        float NoL = saturate(dot(N, L));
+        vec3 H = normalize(L + V);  
 
-        vec3 Ks = u_pointLights[i].color * calculate_cook_torrance(N, V, L, metallic, roughness, F0.r);
-        vec3 Kd = (NoL * (albedo / PI));
+        float NdotL = clamp(dot(N, L), 0.001, 1.0);
+        float NdotV = abs(dot(N, V)) + 0.001;
+        float NdotH = clamp(dot(N, H), 0.0, 1.0);
+        float LdotH = clamp(dot(L, H), 0.0, 1.0);
+        float VdotH = clamp(dot(V, H), 0.0, 1.0);
 
-        //float dist = length(u_pointLights[i].position - v_world_position);
-        //float attenuation = 1.0 / (dist * dist);
+        // vec3 reflection = -normalize(reflect(V, N));
 
-        float attenuation = point_light_attenuation(L, 1.0); // radius is [0, 1]
+        MaterialInfo data = MaterialInfo(
+            NdotL, NdotV, NdotH, LdotH, VdotH,
+            roughness, metallic, specularEnvironmentR0, specularEnvironmentR90, alphaRoughness,
+            diffuseColor, specularColor
+        );
 
-        diffuseContrib += Kd * attenuation;
-        specularContrib += (Ks / PI) * attenuation;
+        // Calculate the shading terms for the microfacet specular shading model
+        vec3 F = specularReflection(data);
+        float G = geometricOcclusion(data);
+        float D = microfacetDistribution(data);
+
+        // Calculation of analytical lighting contribution
+        vec3 diffuseContrib = ((1.0 - F) * (diffuseColor / PI));
+        vec3 specContrib = ((F * G * D) / ((4.0 * NdotL * NdotV) + 0.001));
+        Lo += NdotL * u_pointLights[i].color * (diffuseContrib + specContrib);
     }
 
-#ifdef USE_IMAGE_BASED_LIGHTING
-    float roughness4 = pow(roughness, 4.0);
-
-    // Compute image-based lighting
-    const int NUM_MIP_LEVELS = 7;
-    float mipLevel = NUM_MIP_LEVELS - 1.0 + log2(roughness);
-    vec3 cubemapLookup = fix_cube_lookup(reflect(-V, N), 512, mipLevel);
-
-    irradiance = sRGBToLinear(texture(sc_irradiance, N).rgb, DEFAULT_GAMMA) * u_ambientIntensity;
-    radiance = sRGBToLinear(textureLod(sc_radiance, cubemapLookup, mipLevel).rgb, DEFAULT_GAMMA) * u_ambientIntensity;
-
-    float NoV = saturate(dot(N, V));
-    specularContrib += env_brdf_approx(F0, roughness4, NoV);
-#endif
-
     // Combine direct lighting, IBL, and shadow visbility
-    vec3 Lo = ((diffuseContrib * irradiance) + (specularContrib * radiance)) * (shadowVisibility);
+    // vec3 Lo = ((diffuseContrib * irradiance) + (specularContrib * radiance)) * (shadowVisibility);
     //f_color = vec4(vec3(shadowVisibility), 1.0);
 
     f_color = vec4(Lo, u_opacity); 
