874d1f393da76a530c0c7c5c268b5d663cf84bb6
dimitri <ddiakopoulos@gmail.com>
2017-09-07 14:34:16 -0700

    support for shared asset handles indicating that the asset handle is not the owner

--- scene-editor/assets.hpp
+++ scene-editor/assets.hpp
@@ -67,6 +67,13 @@ public:
         return handle->asset;
     }
 
+    T assign_shared(T asset)
+    {
+        handle->asset = asset;
+        handle->assigned = true;
+        return handle->asset;
+    }
+
     bool assigned() const
     {
         if (handle->assigned) return true;
@@ -91,6 +98,12 @@ template<class T> void global_register_asset(const char * asset_id, T && asset)
 {
     AssetHandle<T> assetHandle(asset_id);
     assetHandle.assign(std::move(asset));
+}
+
+template<class T> void global_register_asset_shared(const char * asset_id, T asset)
+{
+    AssetHandle<T> assetHandle(asset_id);
+    assetHandle.assign_shared(asset);
 }
 
 typedef AssetHandle<GlTexture2D> GlTextureHandle;
