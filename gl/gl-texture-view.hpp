2efacd33a7880db5b302d4ae19ce1c190ae2ae70
dimitri <ddiakopoulos@gmail.com>
2017-11-24 14:39:22 -0800

    gl texture view now has better facilities for visualizing depth buffers

--- gl/gl-texture-view.hpp
+++ gl/gl-texture-view.hpp
@@ -44,12 +44,24 @@ static const char s_textureFrag[] = R"(#version 330
 
 static const char s_textureFragDepth[] = R"(#version 330
     uniform sampler2D u_texture;
+    uniform float u_zNear;
+    uniform float u_zFar;
+
     in vec2 texCoord;
     out vec4 f_color;
+
+    float linear_01_depth(in float z) 
+    {
+        // Used to linearize Z buffer values. x is (1-far/near), y is (far/near), z is (x/far) and w is (y/far).
+        vec4 zBufferParams = vec4(1.0 - u_zFar/u_zNear, u_zFar/u_zNear, 0, 0);
+        return (1.00000 / ((zBufferParams.x * z) + zBufferParams.y ));
+    }
+
     void main()
     {
         vec4 sample = texture(u_texture, texCoord);
-        f_color = vec4(sample.r, sample.r, sample.r, 1.0); 
+        float linearDepthSample = linear_01_depth(sample.r);
+        f_color = vec4(linearDepthSample, linearDepthSample, linearDepthSample, 1.0); 
     }
 )";
 
@@ -86,28 +98,38 @@ namespace avl
     {
         GlShader program;
         GlMesh mesh = make_fullscreen_quad_screenspace();
-        
-        GLTextureView(bool flip = false, bool depthTexture = false)
+        bool hasDepth = false;
+        float2 nearFarDepth;
+
+        GLTextureView(bool flip = false, float2 nearFarDepth = float2(0, 0)) : nearFarDepth(nearFarDepth)
         {
+            if (nearFarDepth.x > 0 || nearFarDepth.y > 0) hasDepth = true;
+
             if (flip)
             {
-                if (depthTexture) program = GlShader(s_textureVertFlip, s_textureFragDepth);
+                if (hasDepth) program = GlShader(s_textureVertFlip, s_textureFragDepth);
                 else program = GlShader(s_textureVertFlip, s_textureFrag);
             }
             else
             {
-                if (depthTexture) program = GlShader(s_textureVert, s_textureFragDepth);
+                if (hasDepth) program = GlShader(s_textureVert, s_textureFragDepth);
                 else program = GlShader(s_textureVert, s_textureFrag);
             }
         }
         
         void draw(const Bounds2D & rect, const float2 windowSize, const GLuint tex)
         {
+
             const float4x4 projection = make_orthographic_matrix(0.0f, windowSize.x, windowSize.y, 0.0f, -1.0f, 1.0f);
             float4x4 model = make_scaling_matrix({ rect.width(), rect.height(), 0.f });
             model = mul(make_translation_matrix({ rect.min().x, rect.min().y, 0.f }), model);
             program.bind();
             program.uniform("u_mvp", mul(projection, model));
+            if (hasDepth)
+            {
+                program.uniform("u_zNear", nearFarDepth.x);
+                program.uniform("u_zFar", nearFarDepth.y);
+            }
             program.texture("u_texture", 0, tex, GL_TEXTURE_2D);
             mesh.draw_elements();
             program.unbind();
