1bd16a7ba8ad570e0afbdfefdc9014c0f7824ba9
dimitri <ddiakopoulos@gmail.com>
2018-01-02 11:31:04 -0800

    add a bit of noise to the terrain scan effect

--- assets/shaders/effects/terrainscan_frag.glsl
+++ assets/shaders/effects/terrainscan_frag.glsl
@@ -28,6 +28,59 @@ in vec3 v_world_position;
 
 out vec4 f_color;
 
+#define NUM_OCTAVES 3
+
+float hash(float n) { return fract(sin(n) * 1e4); }
+float hash(vec2 p) { return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); }
+
+float noise(float x) 
+{
+    float i = floor(x);
+    float f = fract(x);
+    float u = f * f * (3.0 - 2.0 * f);
+    return mix(hash(i), hash(i + 1.0), u);
+}
+
+float noise(vec2 x) 
+{
+    vec2 i = floor(x);
+    vec2 f = fract(x);
+
+    // Four corners in 2D of a tile
+    float a = hash(i);
+    float b = hash(i + vec2(1.0, 0.0));
+    float c = hash(i + vec2(0.0, 1.0));
+    float d = hash(i + vec2(1.0, 1.0));
+
+    // Simple 2D lerp using smoothstep envelope between the values.
+    // return vec3(mix(mix(a, b, smoothstep(0.0, 1.0, f.x)),
+    //          mix(c, d, smoothstep(0.0, 1.0, f.x)),
+    //          smoothstep(0.0, 1.0, f.y)));
+
+    // Same code, with the clamps in smoothstep and common subexpressions
+    // optimized away.
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
+float noise(vec3 x) 
+{
+    const vec3 step = vec3(110, 241, 171);
+
+    vec3 i = floor(x);
+    vec3 f = fract(x);
+ 
+    // For performance, compute the base input to a 1D hash from the integer part of the argument and the 
+    // incremental change to the 1D based on the 3D -> 1D wrapping
+    float n = dot(i, step);
+
+    vec3 u = f * f * (3.0 - 2.0 * f);
+    return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y),
+               mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
+}
+
 // From Unity CgInc: 
 // Used to linearize Z buffer values. x is (1-far/near), y is (far/near), z is (x/far) and w is (y/far).
 float linear_01_depth(in float z) 
@@ -67,9 +120,10 @@ void main()
 
     //if (dist < u_scanDistance && dist > u_scanDistance - u_scanWidth)
     {
+        float n = noise(worldspacePosition) * 0.5;
         float diff = 1 - (u_scanDistance - dist) / (u_scanWidth);
-        vec4 edge = u_leadColor * mix(u_midColor, u_leadColor, pow(diff, u_leadSharp));
-        scannerColor = mix(u_trailColor, edge, diff) + (screenspace_bars(v_texcoord) * u_hbarColor);
+        vec4 edge = clamp((u_leadColor) * mix(u_midColor, u_leadColor, pow(diff, u_leadSharp)), 0, 1);
+        scannerColor = mix(u_trailColor, edge, diff + n) + (screenspace_bars(v_texcoord) * u_hbarColor);
         scannerColor *= diff;
     }
 
